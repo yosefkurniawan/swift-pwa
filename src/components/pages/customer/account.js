c22b28d17d38924092eb984fafdcf1cbf230b33d
yosefkurniawan <jojo@icube.us>
2020-04-01 14:30:25 +0700

    revamp initiation page of Customer Dashboard, Login, and Register to use new layout implementation

--- src/components/pages/customer/account.js
+++ src/components/pages/customer/account.js
@@ -1,9 +1,18 @@
-const Account = (props) => {
-    return (
-        <div>
-            <h1>{props.pageConfig.title}</h1>
-        </div>
-    );
-}
+import Content from "./components/account";
+import Layout from "../../layouts";
+import { withTranslation } from "../../../../i18n";
 
-export default Account;
+const Page = props => {
+    const { t } = props;
+    const pageConfig = {
+        title: t("customer:dashboard:pageTitle"),
+        header: false // available values: "absolute", "relative", false (default)
+    };
+    return (
+        <Layout pageConfig={pageConfig}>
+            <Content {...props} />
+        </Layout>
+    );
+};
+
+export default withTranslation()(Page);
--- src/components/pages/customer/login.js
+++ src/components/pages/customer/login.js
@@ -1,9 +1,18 @@
-const Login = (props) => {
-    return (
-        <div>
-            <h1>{props.pageConfig.title}</h1>
-        </div>
-    );
-}
+import Content from "./components/login";
+import Layout from "../../layouts";
+import { withTranslation } from "../../../../i18n";
 
-export default Login;
+const Page = props => {
+    const { t } = props;
+    const pageConfig = {
+        title: t("customer:login:pageTitle"),
+        header: false // available values: "absolute", "relative", false (default)
+    };
+    return (
+        <Layout pageConfig={pageConfig}>
+            <Content {...props} />
+        </Layout>
+    );
+};
+
+export default withTranslation()(Page);
