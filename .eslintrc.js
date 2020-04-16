39584401eebb5e5f101de5c5fb679e81a526d80f
yosefkurniawan <jojo@icube.us>
2020-04-16 20:46:52 +0700

    fix eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+const projectPath = path.resolve(__dirname, "./");
+
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: 'airbnb',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es6: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    indent: ["error", 4]
-  },
+    extends: 'airbnb',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'indent': ['error', 4],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+        'react/jsx-props-no-spreading': 'off',
+        'max-len': ['error', { code: 150, ignoreComments: true }],
+    },
+    settings: {
+        'import/parser': 'babel-eslint',
+        'import/resolver': 'eslint-import-resolver-babel-module',
+    },
 };
