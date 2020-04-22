1c38c135a5d82cefffef7ec25afbe8a3436e87c6
yosefkurniawan <jojo@icube.us>
2020-04-23 01:09:37 +0700

    26150112 - update calling currency helper

--- src/components/Forms/RangeSlider/index.js
+++ src/components/Forms/RangeSlider/index.js
@@ -3,7 +3,7 @@
 import { Slider, withStyles } from '@material-ui/core';
 import Typography from '@components/Typography';
 import PropTypes from 'prop-types';
-import currency from '@helpers/currency';
+import formatPrice from '@helpers/currency';
 import { sliderStyle, useStyles } from './style';
 
 const CustomSlider = withStyles(sliderStyle)(Slider);
@@ -28,7 +28,7 @@ const RangeSlider = ({
             <div className={styles.spanLabelPrice}>
                 <Typography variant="label" type="regular" letter="uppercase">
                     {
-                        currency({
+                        formatPrice({
                             value: value[0],
                             currency: 'IDR',
                         })
@@ -36,7 +36,7 @@ const RangeSlider = ({
                 </Typography>
                 <Typography variant="label" type="regular" letter="uppercase">
                     {
-                        currency({
+                        formatPrice({
                             value: value[1],
                             currency: 'IDR',
                         })
