1c36ac38b9f32dca89f7b5468731925220ef1fe1
Bimaa Ghafara <bima@icube.us>
2020-04-23 14:26:53 +0700

    separate gqlService for hompage

---
+++ src/pages/home/service/graphql/schema.js
@@ -0,0 +1,63 @@
+import { gql } from 'apollo-boost';
+
+export const getBannerSlider = gql`
+    {
+        getHomepageSlider {
+            slider_id
+            images {
+                image_id
+                image_url
+                mobile_image_url
+                thumb_image_url
+            }
+        }
+    }
+`;
+
+export const getCategoryList = gql`
+    query($url_key: String!) {
+        categoryList(filters: { url_key: { eq: $url_key } }) {
+            id
+            name
+            url_key
+            image
+            products {
+                items {
+                    name
+                    image {
+                        url
+                    }
+                    thumbnail {
+                        url
+                    }
+                    price_range {
+                        minimum_price {
+                            regular_price {
+                                value
+                            }
+                        }
+                    }
+                    price_tiers {
+                        discount {
+                            amount_off
+                            percent_off
+                        }
+                        final_price {
+                            currency
+                            value
+                        }
+                        quantity
+                    }
+                }
+            }
+            children {
+                id
+                name
+                url_key
+                image
+            }
+        }
+    }
+`;
+
+export default { getBannerSlider, getCategoryList };
