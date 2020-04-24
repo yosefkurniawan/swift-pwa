27323b3fb93a489c0760c03e5ca91db80a33618a
Bimaa Ghafara <bima@icube.us>
2020-04-24 16:10:13 +0700

    set fetchPolicy 'network-only' to testing loading in homepage

--- src/pages/home/service/graphql/index.js
+++ src/pages/home/service/graphql/index.js
@@ -1,8 +1,19 @@
 import { useQuery } from '@apollo/react-hooks';
 import * as Schema from './schema';
 
-export const getBannerSlider = () => useQuery(Schema.getBannerSlider);
-export const getFeaturedProducts = (variables) => useQuery(Schema.getFeaturedProducts, { variables });
-export const getCategoryList = (variables) => useQuery(Schema.getCategoryList, { variables });
+let fetchPolicy = 'cache-first';
+fetchPolicy = 'network-only'; // comment this line to test loader/skeleton component
+
+export const getBannerSlider = () => useQuery(Schema.getBannerSlider, {
+    fetchPolicy,
+});
+export const getFeaturedProducts = (variables) => useQuery(Schema.getFeaturedProducts, {
+    variables,
+    fetchPolicy,
+});
+export const getCategoryList = (variables) => useQuery(Schema.getCategoryList, {
+    variables,
+    fetchPolicy,
+});
 
 export default { getCategoryList, getBannerSlider, getFeaturedProducts };
