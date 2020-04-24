7d79cb35251c1e34b444308f08d16f90190aa720
Bimaa Ghafara <bima@icube.us>
2020-04-24 14:21:24 +0700

    update schema getFeaturedProducts, link for category banner

--- src/pages/home/service/graphql/index.js
+++ src/pages/home/service/graphql/index.js
@@ -2,6 +2,7 @@ import { useQuery } from '@apollo/react-hooks';
 import * as Schema from './schema';
 
 export const getBannerSlider = () => useQuery(Schema.getBannerSlider);
+export const getFeaturedProducts = (variables) => useQuery(Schema.getFeaturedProducts, { variables });
 export const getCategoryList = (variables) => useQuery(Schema.getCategoryList, { variables });
 
-export default { getCategoryList, getBannerSlider };
+export default { getCategoryList, getBannerSlider, getFeaturedProducts };
--- src/pages/home/service/graphql/schema.js
+++ src/pages/home/service/graphql/schema.js
@@ -14,6 +14,69 @@ export const getBannerSlider = gql`
     }
 `;
 
+
+export const getFeaturedProducts = gql`
+    query($url_key: String!) {
+        categoryList(filters: { url_key: { eq: $url_key } }) {
+            description
+            name
+            url_key
+            image
+            title_rewrite
+            meta_title
+            children {
+                id
+                level
+                name
+                title_rewrite
+                path
+                description
+                image
+                url_path
+                url_key
+                products {
+                    items {
+                        __typename
+                        name
+                        url_key
+                        canonical_url
+                        image {
+                            url
+                        }
+                        thumbnail {
+                            url
+                        }
+                        price_tiers {
+                            discount {
+                                amount_off
+                                percent_off
+                            }
+                            final_price {
+                                currency
+                                value
+                            }
+                            quantity
+                        }
+                        price_range {
+                            minimum_price {
+                                regular_price {
+                                    currency
+                                    value
+                                }
+                                final_price {
+                                    currency
+                                    value
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            children_count
+        }
+    }
+`;
+
 export const getCategoryList = gql`
     query($url_key: String!) {
         categoryList(filters: { url_key: { eq: $url_key } }) {
@@ -69,4 +132,4 @@ export const getCategoryList = gql`
     }
 `;
 
-export default { getBannerSlider, getCategoryList };
+export default { getBannerSlider, getCategoryList, getFeaturedProducts };
