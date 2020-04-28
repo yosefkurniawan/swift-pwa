4a4ee00a8d582ca29db9bcf2250dc268b3983c7a
Asih Priyatno <asih@icube.us>
2020-04-28 16:07:47 +0700

    feat(Search Page): Tabs
    show tabs category if have product #26146935

--- src/components/ProductList/component/index.js
+++ src/components/ProductList/component/index.js
@@ -21,7 +21,10 @@ const getCategoryFromAgregations = (agg) => {
         if (agg[index].field === 'category_id') {
             // eslint-disable-next-line no-plusplus
             for (let catIdx = 0; catIdx < agg[index].value.length; catIdx++) {
-                category.push({ label: agg[index].value[catIdx].label, value: agg[index].value[catIdx].value });
+                // only if have image show category
+                if (agg[index].value[catIdx].count > 0) {
+                    category.push({ label: agg[index].value[catIdx].label, value: agg[index].value[catIdx].value });
+                }
             }
         }
     }
