8e1001933095f1ae7687939e097e7ce431694fa7
Bimaa Ghafara <bima@icube.us>
2020-04-29 13:58:06 +0700

    fix styling about-us

--- src/pages/catalog/pages/aboutUs/components.js
+++ src/pages/catalog/pages/aboutUs/components.js
@@ -1,26 +1,19 @@
 import Loading from '@components/Loaders';
-import { makeStyles } from '@material-ui/core/styles';
 import { getCmsPage } from './services/graphql';
 
-const useStyles = makeStyles(() => ({
-    container: {
-        padding: '0 16px',
-        textAlign: 'justify',
-    },
-}));
-
 const AboutUsPage = () => {
-    const styles = useStyles();
     const { error, loading, data } = getCmsPage({ identifier: 'about-us' });
     if (error) return <p>error</p>;
     if (loading) return <Loading size="40px" />;
 
     return (
-        <div className={styles.container}>
-            {/* <h1>About us</h1> */}
-
+        <>
             <style>
                 {`
+                    .cms-container {
+                        padding: 0 16px;
+                        text-align: justify;
+                    }
                     .cms-content-important {
                         background: #f5f5f5;
                         padding: 16px;
@@ -30,8 +23,8 @@ const AboutUsPage = () => {
             </style>
 
             {/* eslint-disable-next-line react/no-danger */}
-            <div dangerouslySetInnerHTML={{ __html: data.cmsPage.content }} />
-        </div>
+            <div className="cms-container" dangerouslySetInnerHTML={{ __html: data.cmsPage.content }} />
+        </>
     );
 };
 
