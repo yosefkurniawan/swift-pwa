50f704f37b61f12252dc2c772d408f5e2d6e7823
Bimaa Ghafara <bima@icube.us>
2020-04-30 11:22:41 +0700

    move cms styles to src/styles/index

--- src/pages/catalog/pages/cms/components.js
+++ src/pages/catalog/pages/cms/components.js
@@ -3,26 +3,6 @@ const CmsPage = (props) => {
 
     return (
         <>
-            <style>
-                {`
-                    .cms-container {
-                        padding: 0 16px;
-                        text-align: justify;
-                    }
-                    .cms-container .cms-content-important {
-                        background: #f5f5f5;
-                        padding: 16px;
-                        border-radius: 8px;
-                    }
-                    .cms-container li a {
-                        color: #006bb4
-                    }
-                    .cms-container li a:hover, .cms-container li a:active {
-                        color: #444
-                    }
-                `}
-            </style>
-
             {/* eslint-disable-next-line react/no-danger */}
             <div className="cms-container" dangerouslySetInnerHTML={{ __html: content }} />
         </>
