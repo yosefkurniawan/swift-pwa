ef30b0b9e2a6a962c3701ac56e792d69963c9146
Bimaa Ghafara <bima@icube.us>
2020-05-04 13:40:42 +0700

    add mutation contactusFormSubmit

--- src/pages/contact/services/graphql/index.js
+++ src/pages/contact/services/graphql/index.js
@@ -1,6 +1,8 @@
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import * as Schema from './schema';
 
 export const getContactPage = (variables) => useQuery(Schema.getContactPage, { variables });
 
-export default { getContactPage };
+export const contactusFormSubmit = (variables) => useMutation(Schema.contactusFormSubmit, { variables });
+
+export default { getContactPage, contactusFormSubmit };
