9f23d3ff94b6db0e381f0de3992ceddd6e6f33c4
Asih Priyatno <asih@icube.us>
2020-05-12 12:50:02 +0700

    add get token and client

--- src/api/graphql-request/index.js
+++ src/api/graphql-request/index.js
@@ -1,13 +1,15 @@
 const { GraphQLClient } = require('graphql-request');
+const { decrypt } = require('../helper/encryption');
 
 function requestGraph(query, variables = {}, context = {}) {
     return new Promise((resolve) => {
         const headers = {
-            Authorization: context.session.token ? `Bearer ${context.session.token}` : '',
+            Authorization: context.session.token ? `Bearer ${decrypt(context.session.token)}` : '',
         };
         const client = new GraphQLClient('https://swiftpwa-be.testingnow.me/graphql', {
             headers,
         });
+        console.log(client);
         client.request(query, variables).then((data) => resolve(data)).catch((err) => resolve(err));
     });
 }
