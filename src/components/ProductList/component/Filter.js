a2cca2718c533245a659e207bf7d5769b52bb328
yosefkurniawan <jojo@icube.us>
2020-05-12 14:14:51 +0700

    26146934 - add isSearch flag to display related sortby

--- src/components/ProductList/component/Filter.js
+++ src/components/ProductList/component/Filter.js
@@ -3,30 +3,44 @@ import React from 'react';
 import FilterDialog from '@components/FilterDialog';
 import PropTypes from 'prop-types';
 
-
-const sortByData = [
-    { value: JSON.stringify({ key: 'relevance', value: 'DESC' }), label: 'Relevance' },
-    { value: JSON.stringify({ key: 'name', value: 'ASC' }), label: 'Alphabetically (A to Z)' },
-    { value: JSON.stringify({ key: 'name', value: 'DESC' }), label: 'Alphabetically (Z to A)' },
-    { value: JSON.stringify({ key: 'price', value: 'ASC' }), label: 'Price (Low to High)' },
-    { value: JSON.stringify({ key: 'price', value: 'DESC' }), label: 'Price (High to Low)' },
-    { value: JSON.stringify({ key: 'position', value: 'DESC' }), label: 'New Item' },
-];
-
 const Filter = ({
-    openFilter, setOpenFilter, setFilter, defaultValue = {}, elastic, filter,
-}) => (
-    <FilterDialog
-        defaultValue={defaultValue}
-        open={openFilter}
-        sortByData={sortByData}
-        elastic={elastic}
-        setOpen={() => setOpenFilter(!openFilter)}
-        loading={filter.loading}
-        data={filter}
-        getValue={(v) => setFilter(v)}
-    />
-);
+    openFilter, setOpenFilter, setFilter, defaultValue = {}, elastic, filter, isSearch = false,
+}) => {
+    console.log('qweqe');
+    console.log(isSearch);
+    let sortByData;
+
+    if (isSearch) {
+        sortByData = [
+            { value: JSON.stringify({ key: 'relevance', value: 'DESC' }), label: 'Relevance' },
+            { value: JSON.stringify({ key: 'name', value: 'ASC' }), label: 'Alphabetically (A to Z)' },
+            { value: JSON.stringify({ key: 'name', value: 'DESC' }), label: 'Alphabetically (Z to A)' },
+            { value: JSON.stringify({ key: 'price', value: 'ASC' }), label: 'Price (Low to High)' },
+            { value: JSON.stringify({ key: 'price', value: 'DESC' }), label: 'Price (High to Low)' },
+        ];
+    } else {
+        sortByData = [
+            { value: JSON.stringify({ key: 'position', value: 'ASC' }), label: 'Most Relevance' },
+            { value: JSON.stringify({ key: 'name', value: 'ASC' }), label: 'Alphabetically (A to Z)' },
+            { value: JSON.stringify({ key: 'name', value: 'DESC' }), label: 'Alphabetically (Z to A)' },
+            { value: JSON.stringify({ key: 'price', value: 'ASC' }), label: 'Price (Low to High)' },
+            { value: JSON.stringify({ key: 'price', value: 'DESC' }), label: 'Price (High to Low)' },
+        ];
+    }
+
+    return (
+        <FilterDialog
+            defaultValue={defaultValue}
+            open={openFilter}
+            sortByData={sortByData}
+            elastic={elastic}
+            setOpen={() => setOpenFilter(!openFilter)}
+            loading={filter.loading}
+            data={filter}
+            getValue={(v) => setFilter(v)}
+        />
+    );
+};
 
 Filter.propTypes = {
     setFilter: PropTypes.func.isRequired,
