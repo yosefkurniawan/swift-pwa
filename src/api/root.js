81010d42e55abe54b8c6ec5e0a0156b40c4a07a9
Asih Priyatno <asih@icube.us>
2020-05-13 15:29:08 +0700

    implement update and delete item from internal request

--- src/api/root.js
+++ src/api/root.js
@@ -1,7 +1,11 @@
 const generateCustomerToken = require('./resolver/createCustomerToken');
 const revokeCustomerToken = require('./resolver/revokeCustomerToken');
+const updateCartItems = require('./resolver/updateCartItems');
+const removeItemFromCart = require('./resolver/removeItemFromCart');
 
 module.exports = {
     generateCustomerToken,
     revokeCustomerToken,
+    updateCartItems,
+    removeItemFromCart,
 };
