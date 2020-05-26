3253b5f77dbcdacd567cc22ea0146de0427740ad
Asih Priyatno <asih@icube.us>
2020-05-26 15:03:08 +0700

    add skeleton loading

--- src/pages/customer/storeCredit/component/index.js
+++ src/pages/customer/storeCredit/component/index.js
@@ -8,6 +8,7 @@ import Paper from '@material-ui/core/Paper';
 import TableContainer from '@material-ui/core/TableContainer';
 import Link from 'next/link';
 import useStyles from '../style';
+import SkeletonStoreCredit from './skeleton';
 
 const StoreCreditPage = ({ t }) => {
     const styles = useStyles();
@@ -23,12 +24,13 @@ const StoreCreditPage = ({ t }) => {
         setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
     };
+    const loading = true;
     return (
         <div className={styles.container}>
             <p className={styles.textBalance}>
                 {t('customer:storeCredit:balance')}
                 {' '}
-                <b>Rp. 10.000</b>
+                <b>{loading ? '0' : 'Rp. 10.000' }</b>
             </p>
 
             <div className={styles.tableContainer}>
@@ -45,28 +47,33 @@ const StoreCreditPage = ({ t }) => {
 
                         </TableHead>
                         <TableBody>
-                            <TableRow>
-                                <TableCell align="left">1</TableCell>
-                                <TableCell align="left"><span>+$50.00</span></TableCell>
-                                <TableCell align="left">$150.00</TableCell>
-                                <TableCell align="left">topup balance</TableCell>
-                                <TableCell align="left">May 5, 2020</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TablePagination
-                                    rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                                    colSpan={5}
-                                    count={20}
-                                    rowsPerPage={rowsPerPage}
-                                    page={page}
-                                    SelectProps={{
-                                        inputProps: { 'aria-label': 'rows per page' },
-                                        native: true,
-                                    }}
-                                    onChangePage={handleChangePage}
-                                    onChangeRowsPerPage={handleChangeRowsPerPage}
-                                />
-                            </TableRow>
+                            {loading ? <SkeletonStoreCredit /> : (
+                                <>
+                                    <TableRow>
+                                        <TableCell align="left">1</TableCell>
+                                        <TableCell align="left"><span>+$50.00</span></TableCell>
+                                        <TableCell align="left">$150.00</TableCell>
+                                        <TableCell align="left">topup balance</TableCell>
+                                        <TableCell align="left">May 5, 2020</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TablePagination
+                                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                                            colSpan={5}
+                                            count={20}
+                                            rowsPerPage={rowsPerPage}
+                                            page={page}
+                                            SelectProps={{
+                                                inputProps: { 'aria-label': 'rows per page' },
+                                                native: true,
+                                            }}
+                                            onChangePage={handleChangePage}
+                                            onChangeRowsPerPage={handleChangeRowsPerPage}
+                                        />
+                                    </TableRow>
+                                </>
+                            )}
+
                         </TableBody>
                     </Table>
                 </TableContainer>
