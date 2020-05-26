71c07cb36dd3e04ebee4097000f5d5788ab327b7
Bimaa Ghafara <bima@icube.us>
2020-05-27 06:55:48 +0700

    on progress customerNotificationList page

--- src/pages/customer/notification/component.js
+++ src/pages/customer/notification/component.js
@@ -1,7 +1,32 @@
-const Notification = () => (
-    <>
-        Notification
-    </>
-);
+import { List, ListItem, ListItemText } from '@material-ui/core';
+import gqlService from './service/graphql';
+
+const Notification = () => {
+    const { loading, data, error } = gqlService.customerNotificationList();
+
+    if (loading) return <p> Loading......</p>;
+    if (error) return <p>{`Error: ${error.message}`}</p>;
+    if (!data) return <p>Not found</p>;
+
+    console.log(data);
+    const localDateString = (stringTime) => new Date(stringTime).toLocaleDateString(
+        {},
+        {
+            weekday: 'short', year: 'numeric', month: 'long', day: 'numeric',
+        },
+    );
+
+    return (
+        <div className="container" style={{ paddingTop: 0 }}>
+            <List>
+                {data.customerNotificationList.items.map((item) => (
+                    <ListItem>
+                        <ListItemText primary={item.subject} secondary={localDateString(item.createdAt)} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+};
 
 export default Notification;
