db9ffc85fc4517700c58df22aa15fcfd99627ac5
Bimaa Ghafara <bima@icube.us>
2020-05-27 10:48:06 +0700

    update Typography and handleItemClick

--- src/pages/customer/notification/component.js
+++ src/pages/customer/notification/component.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-console */
 import { List, ListItem, ListItemText } from '@material-ui/core';
+import Typography from '@components/Typography';
 import gqlService from './service/graphql';
 
 const Notification = () => {
@@ -16,13 +18,30 @@ const Notification = () => {
         },
     );
 
+    const handleItemClick = (item) => {
+        console.log(item);
+    };
+
     return (
         <div className="container" style={{ paddingTop: 0 }}>
             <List>
                 {data.customerNotificationList.items.map((item) => (
-                    <ListItem>
-                        <ListItemText primary={item.subject} secondary={localDateString(item.createdAt)} />
-                    </ListItem>
+                    <>
+                        <ListItem divider button onClick={() => handleItemClick(item)}>
+                            <ListItemText
+                                primary={(
+                                    <Typography variant="label" type={item.unread ? 'bold' : 'regular'} size="14">
+                                        {item.subject}
+                                    </Typography>
+                                )}
+                                secondary={(
+                                    <Typography variant="label" type={item.unread ? 'bold' : 'regular'}>
+                                        {localDateString(item.createdAt)}
+                                    </Typography>
+                                )}
+                            />
+                        </ListItem>
+                    </>
                 ))}
             </List>
         </div>
