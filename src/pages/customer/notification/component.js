945c432d7f66be56ca9ce01598ced1358575c8de
Bimaa Ghafara <bima@icube.us>
2020-05-27 14:21:01 +0700

    fix list key

--- src/pages/customer/notification/component.js
+++ src/pages/customer/notification/component.js
@@ -25,23 +25,21 @@ const Notification = () => {
     return (
         <div className="container" style={{ paddingTop: 0 }}>
             <List>
-                {data.customerNotificationList.items.map((item) => (
-                    <>
-                        <ListItem divider button onClick={() => handleItemClick(item)}>
-                            <ListItemText
-                                primary={(
-                                    <Typography variant="label" type={item.unread ? 'bold' : 'regular'} size="14">
-                                        {item.subject}
-                                    </Typography>
-                                )}
-                                secondary={(
-                                    <Typography variant="label" type={item.unread ? 'bold' : 'regular'}>
-                                        {localDateString(item.createdAt)}
-                                    </Typography>
-                                )}
-                            />
-                        </ListItem>
-                    </>
+                {data.customerNotificationList.items.map((item, i) => (
+                    <ListItem key={i} divider button onClick={() => handleItemClick(item)}>
+                        <ListItemText
+                            primary={(
+                                <Typography variant="label" type={item.unread ? 'bold' : 'regular'} size="14">
+                                    {item.subject}
+                                </Typography>
+                            )}
+                            secondary={(
+                                <Typography variant="label" type={item.unread ? 'bold' : 'regular'}>
+                                    {localDateString(item.createdAt)}
+                                </Typography>
+                            )}
+                        />
+                    </ListItem>
                 ))}
             </List>
         </div>
