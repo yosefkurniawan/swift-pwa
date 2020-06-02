d00a360593e82654e471af30bc756efc25aafe99
rifqi3004 <rifqiattarbani@gmail.com>
2020-06-02 09:45:38 +0700

    add query graphql blog

---
+++ src/pages/catalog/pages/blog/services/graphql/index.js
@@ -0,0 +1,17 @@
+import { useQuery } from '@apollo/react-hooks';
+import * as schema from './schema';
+
+export const getBlog = (variables) => useQuery(schema.getAllPost, {
+    variables,
+});
+
+export const getCategory = () => useQuery(schema.getCategory);
+
+export const getDetailBlog = (id) => useQuery(schema.getPostById, {
+    variables: {
+        id,
+    },
+});
+
+
+export default {};
--- src/pages/catalog/pages/blog/services/graphql/schema.js
+++ src/pages/catalog/pages/blog/services/graphql/schema.js
@@ -1,10 +1,35 @@
 import { gql } from 'apollo-boost';
 
-export const getCategory = gql``;
+export const getCategory = gql`
+    {
+        getBlogCategory {
+            data {
+                name
+                description
+                id
+                image_alt
+                image_file_name
+                image_title
+                is_description_enabled
+                url_key
+                path
+                parent_id
+                meta_description
+                meta_keywords
+                meta_prefix
+                meta_title
+                sort_order
+                status
+                created_at
+                updated_at
+            }
+        }
+    }
+`;
 
 export const getAllPost = gql`
-    {
-        getBlogByFilter {
+    query getAllBlog($category_id: Int){
+        getBlogByFilter(category_id: $category_id) {
             data {
                 id
                 title
@@ -22,25 +47,24 @@ export const getAllPost = gql`
 `;
 
 export const getPostById = gql`
-query getBlog($id: Int, $category_id: Int) {
-    getBlogByFilter(id: $id, category_id: $category_id){
-      data {
-        id
-        title
-        canonical_category_id
-        author_id
-        url_key
-        short_content
-        publish_date
-        featured_image_url
-        canonical_category_id
-        featured_image_alt
-        content
-        created_at
-
-      }
+    query getBlog($id: Int) {
+        getBlogByFilter(id: $id) {
+            data {
+                id
+                title
+                canonical_category_id
+                author_id
+                url_key
+                short_content
+                publish_date
+                featured_image_url
+                canonical_category_id
+                featured_image_alt
+                content
+                created_at
+            }
+        }
     }
-  }
 `;
 
 export default {};
