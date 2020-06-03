e05ee5fe80f3f2d715830a8d5a6dbbd2a4353cd7
Asih Priyatno <asih@icube.us>
2020-06-03 09:22:57 +0700

    fix error if select payment method

--- src/pages/checkout/components/PaymentList/index.js
+++ src/pages/checkout/components/PaymentList/index.js
@@ -51,61 +51,63 @@ export default function CustomizedExpansionPanels({
     let content;
 
     const handlePayment = async (val) => {
-        const { cart } = checkout.data;
-        let state = { ...checkout };
-        state.selected.payment = val;
-        state.status.backdrop = true;
-        setCheckout(state);
+        if (val) {
+            const { cart } = checkout.data;
+            let state = { ...checkout };
+            state.selected.payment = val;
+            state.status.backdrop = true;
+            setCheckout(state);
 
-        const result = await setPaymentMethod({ variables: { cartId: cart.id, code: val } });
+            const result = await setPaymentMethod({ variables: { cartId: cart.id, code: val } });
 
-        state = { ...checkout };
+            state = { ...checkout };
 
-        if (result) {
-            state.data.cart = result.data.setPaymentMethodOnCart.cart;
-            updateFormik(result.data.setPaymentMethodOnCart.cart);
-        } else {
-            handleOpenMessage({
-                variant: 'error',
-                text: t('checkout:message:problemConnection'),
+            if (result) {
+                state.data.cart = result.data.setPaymentMethodOnCart.cart;
+                updateFormik(result.data.setPaymentMethodOnCart.cart);
+            } else {
+                handleOpenMessage({
+                    variant: 'error',
+                    text: t('checkout:message:problemConnection'),
+                });
+            }
+            state.status.backdrop = false;
+            setCheckout(state);
+
+            const selectedPayment = data.paymentMethod.filter((item) => item.code === val);
+            const dataLayer = {
+                event: 'checkout',
+                ecommerce: {
+                    checkout: {
+                        actionField: { step: 3, option: selectedPayment[0].title, action: 'checkout' },
+                        products: cart.items.map(({ quantity, product, prices }) => ({
+                            name: product.name,
+                            id: product.sku,
+                            price: prices.price.value,
+                            category: product.categories.length > 0 ? product.categories[0].name : '',
+                            list: product.categories.length > 0 ? product.categories[0].name : '',
+                            quantity,
+                        })),
+                    },
+                    currencyCode: storeConfig.base_currency_code || 'IDR',
+                },
+            };
+            const dataLayerOption = {
+                event: 'checkoutOption',
+                ecommerce: {
+                    currencyCode: storeConfig.base_currency_code || 'IDR',
+                    checkout_option: {
+                        actionField: { step: 3, option: selectedPayment[0].title, action: 'checkout_option' },
+                    },
+                },
+            };
+            TagManager.dataLayer({
+                dataLayer,
+            });
+            TagManager.dataLayer({
+                dataLayer: dataLayerOption,
             });
         }
-        state.status.backdrop = false;
-        setCheckout(state);
-
-        const selectedPayment = data.paymentMethod.filter((item) => item.code === val);
-        const dataLayer = {
-            event: 'checkout',
-            ecommerce: {
-                checkout: {
-                    actionField: { step: 3, option: selectedPayment[0].title, action: 'checkout' },
-                    products: cart.items.map(({ quantity, product, prices }) => ({
-                        name: product.name,
-                        id: product.sku,
-                        price: prices.price.value,
-                        category: product.categories.length > 0 ? product.categories[0].name : '',
-                        list: product.categories.length > 0 ? product.categories[0].name : '',
-                        quantity,
-                    })),
-                },
-                currencyCode: storeConfig.base_currency_code || 'IDR',
-            },
-        };
-        const dataLayerOption = {
-            event: 'checkoutOption',
-            ecommerce: {
-                currencyCode: storeConfig.base_currency_code || 'IDR',
-                checkout_option: {
-                    actionField: { step: 3, option: selectedPayment[0].title, action: 'checkout_option' },
-                },
-            },
-        };
-        TagManager.dataLayer({
-            dataLayer,
-        });
-        TagManager.dataLayer({
-            dataLayer: dataLayerOption,
-        });
     };
 
     const handleChange = (panel) => (event, newExpanded) => {
