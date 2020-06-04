1f5563a3842a4c6bf6b7ab6d344e51238157f2dd
Asih Priyatno <asih@icube.us>
2020-06-04 13:27:34 +0700

    add request to graphql

--- src/pages/brands/component/featured/index.js
+++ src/pages/brands/component/featured/index.js
@@ -1,51 +1,9 @@
 import Carousel from '@components/Slider/Carousel';
 import ItemFeatured from './Item';
 
-const featured = [
-    {
-        attribute_id: 5643,
-        logo: 'https://swiftpwa-be.testingnow.me/media/brand/a/d/adidas.jpg',
-        name: 'Adidas',
-        sort_order: 0,
-        is_active: 1,
-        category_url: 'brands/adidas.html',
-    },
-    {
-        attribute_id: 5644,
-        logo: 'https://swiftpwa-be.testingnow.me/media/brand/n/i/nike.png',
-        name: 'Nike',
-        sort_order: 0,
-        is_active: 1,
-        category_url: 'brands/nike.html',
-    },
-    {
-        attribute_id: 5645,
-        logo: 'https://swiftpwa-be.testingnow.me/media/brand/u/n/uniqlo.png',
-        name: 'Uniqlo',
-        sort_order: 0,
-        is_active: 1,
-        category_url: 'brands/uniqlo.html',
-    },
-    {
-        attribute_id: 5650,
-        logo: 'https://swiftpwa-be.testingnow.me/media/brand/z/a/zara.png',
-        name: 'Zara',
-        sort_order: 0,
-        is_active: 1,
-        category_url: 'brands/zara.html',
-    },
-    {
-        attribute_id: 5651,
-        logo: 'https://swiftpwa-be.testingnow.me/media/brand/h/m/hm.png',
-        name: 'H&M',
-        sort_order: 0,
-        is_active: 1,
-        category_url: 'brands/hm.html',
-    },
-];
 
-const FeaturedBrands = () => (
-    <Carousel title="Featured Brands" data={featured} customItem={ItemFeatured} />
+const FeaturedBrands = ({ data = [] }) => (
+    <Carousel title="Featured Brands" data={data} customItem={ItemFeatured} />
 );
 
 export default FeaturedBrands;
--- src/pages/brands/component/index.js
+++ src/pages/brands/component/index.js
@@ -1,9 +1,18 @@
 import FeaturedBrands from './featured';
+import { getBrands } from '../services';
 
-const ComponentBrands = () => (
-    <div>
-        <FeaturedBrands />
-    </div>
-);
+const ComponentBrands = () => {
+    const { data, loading } = getBrands({ pageSize: 100, currentPage: 1 });
+    if (loading) {
+        return <div>loading</div>;
+    }
+
+    const { getBrandList } = data;
+    return (
+        <div>
+            <FeaturedBrands data={getBrandList.featured} />
+        </div>
+    );
+};
 
 export default ComponentBrands;
