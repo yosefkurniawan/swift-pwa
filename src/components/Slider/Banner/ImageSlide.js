8bcf37852b522c110c1c5d65d01c6e600ce8c31d
Bimaa Ghafara <bima@icube.us>
2020-06-04 15:31:52 +0700

    update slug url for banner slider

--- src/components/Slider/Banner/ImageSlide.js
+++ src/components/Slider/Banner/ImageSlide.js
@@ -3,11 +3,22 @@ import Link from 'next/link';
 import useStyles from './style';
 import setDefaultWhenEmpty from '../../../helpers/checkImageSrc';
 
-const ImageSlide = ({ imageUrl = '', link = '#' }) => {
+
+/**
+ slug page need props 'href' & 'as' to prevent browser reloading
+ *isSlug == true => <link href="/[...slug]" as={link} />
+ *isSlug == false => <link href={link} />
+*/
+
+const ImageSlide = ({ imageUrl = '', link = '#', isSlug = true }) => {
     const styles = useStyles();
+    const href = link && link[0] === '/' ? link : `/${link}`;
     return (
-        <Link href={link}>
-            <img src={setDefaultWhenEmpty(imageUrl)} alt={link} className={styles.imageSlider} />
+        <Link
+            href={isSlug ? '/[...slug]' : href}
+            {...(isSlug && { as: href })}
+        >
+            <img src={setDefaultWhenEmpty(imageUrl)} alt={href} className={styles.imageSlider} />
         </Link>
     );
 };
