e7bca01e55010860ba0ddeec462ab2e7030e5dbd
Bimaa Ghafara <bima@icube.us>
2020-06-19 15:54:19 +0700

    onprogress skeleteon category page, add hidden-... class

--- src/pages/slug/pages/category/components/Skeleton.js
+++ src/pages/slug/pages/category/components/Skeleton.js
@@ -1,23 +1,45 @@
-import Skeleton from '@material-ui/lab/Skeleton';
+import Skeleton from '@components/Skeleton';
+import { Grid } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import ProductItemLoading from '@components/ProductItem/component/Skeleton';
-import GridList from '@components/GridList';
+// import GridList from '@components/GridList';
 import useStyles from '../style';
 
 const SkeletonCategory = () => {
     const styles = useStyles();
     return (
         <Box className={styles.container}>
-            <Skeleton variant="rect" className={styles.skeletonBanner} />
+            <Skeleton
+                variant="rect"
+                xsStyle={{ width: '100vw', height: '60vw' }}
+                mdStyle={{ width: '960px', height: '577px' }}
+            />
             <Skeleton variant="text" width={225} />
             <Skeleton variant="text" width="100%" />
             <Skeleton variant="text" width="100%" />
             <Skeleton variant="text" width="40%" style={{ marginBottom: 20 }} />
-            <GridList
-                data={[1, 1, 1, 1]}
+            {/* <GridList
+                data={[1, 1, 1, 1, 1, 1]}
                 ItemComponent={ProductItemLoading}
                 gridItemProps={{ xs: 6, sm: 4, md: 3 }}
-            />
+            /> */}
+            <Grid
+                container
+                spacing={1}
+            >
+                {[1, 2, 3, 4, 5, 6, 7, 8].map((el) => (
+                    <Grid
+                        key={el}
+                        item
+                        xs={6}
+                        sm={4}
+                        md={3}
+                        className={el <= 2 ? 'hidden-xs hidden-sm' : ''}
+                    >
+                        <ProductItemLoading />
+                    </Grid>
+                ))}
+            </Grid>
         </Box>
     );
 };
--- src/pages/slug/pages/category/style.js
+++ src/pages/slug/pages/category/style.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core';
 import { GRAY_PRIMARY, WHITE } from '@theme/colors';
 import { FlexColumn } from '@theme/mixins';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     container: {
         width: '100%',
         height: '100%',
@@ -26,14 +26,6 @@ const useStyles = makeStyles((theme) => ({
     },
     breadcrumbs: {
         padding: 15,
-    },
-    skeletonBanner: {
-        width: '100vw',
-        height: '60vw',
-        [theme.breakpoints.up('md')]: {
-            width: '960px',
-            height: '577px',
-        },
     },
 }));
 
