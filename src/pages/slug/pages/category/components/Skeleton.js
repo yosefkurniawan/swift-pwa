ca1f133ba4b59faf4d35cd96713bedfc61727a59
Bimaa Ghafara <bima@icube.us>
2020-06-19 17:29:29 +0700

    create ProductListSkeleton

--- src/pages/slug/pages/category/components/Skeleton.js
+++ src/pages/slug/pages/category/components/Skeleton.js
@@ -1,12 +1,11 @@
 import Skeleton from '@components/Skeleton';
-import { Grid } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
-import ProductItemLoading from '@components/ProductItem/component/Skeleton';
-// import GridList from '@components/GridList';
+import ProductListSkeleton from '@components/ProductList/component/Skeleton';
 import useStyles from '../style';
 
 const SkeletonCategory = () => {
     const styles = useStyles();
+
     return (
         <Box className={styles.container}>
             <Skeleton
@@ -18,28 +17,7 @@ const SkeletonCategory = () => {
             <Skeleton variant="text" width="100%" />
             <Skeleton variant="text" width="100%" />
             <Skeleton variant="text" width="40%" style={{ marginBottom: 20 }} />
-            {/* <GridList
-                data={[1, 1, 1, 1, 1, 1]}
-                ItemComponent={ProductItemLoading}
-                gridItemProps={{ xs: 6, sm: 4, md: 3 }}
-            /> */}
-            <Grid
-                container
-                spacing={1}
-            >
-                {[1, 2, 3, 4, 5, 6, 7, 8].map((el) => (
-                    <Grid
-                        key={el}
-                        item
-                        xs={6}
-                        sm={4}
-                        md={3}
-                        className={el <= 2 ? 'hidden-xs hidden-sm' : ''}
-                    >
-                        <ProductItemLoading />
-                    </Grid>
-                ))}
-            </Grid>
+            <ProductListSkeleton />
         </Box>
     );
 };
