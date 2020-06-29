2d43731920eecbc813c0fb341ac280be9299563f
Bimaa Ghafara <bima@icube.us>
2020-06-29 10:10:12 +0700

    onprogress lazy image

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,6 +1,4 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-param-reassign */
-import Skeleton from '@material-ui/lab/Skeleton';
 import useStyles from './style';
 
 const Image = ({
@@ -19,9 +17,8 @@ const Image = ({
     const imageRef = React.useRef();
 
     function on(el, eventName, callback, opts) {
-        opts = opts || false;
         if (el.addEventListener) {
-            el.addEventListener(eventName, callback, opts);
+            el.addEventListener(eventName, callback, opts || false);
         } else if (el.attachEvent) {
             el.attachEvent(`on${eventName}`, (e) => {
                 callback.call(el, e || window.event);
@@ -30,25 +27,17 @@ const Image = ({
     }
 
     function off(el, eventName, callback, opts) {
-        opts = opts || false;
         if (el.removeEventListener) {
-            el.removeEventListener(eventName, callback, opts);
+            el.removeEventListener(eventName, callback, opts || false);
         } else if (el.detachEvent) {
             el.detachEvent(`on${eventName}`, callback);
         }
     }
 
     const isVisible = (element) => {
-        // const windowInnerWidth = window.innerWidth || document.documentElement.clientWidth;
         const windowInnerHeight = window.innerHeight || document.documentElement.clientHeight;
         const check = (position, base) => element.getBoundingClientRect()[position] >= 0 && element.getBoundingClientRect()[position] < base;
-        // const visibilityX = check('left', windowInnerWidth) || check('right', windowInnerWidth);
         const visibilityY = check('top', windowInnerHeight) || check('bottom', windowInnerHeight);
-        console.log(element);
-        console.log(element.getBoundingClientRect());
-        // console.log(visibilityX);
-        console.log(visibilityY);
-        // return visibilityX && visibilityY;
         return visibilityY;
     };
 
@@ -81,58 +70,28 @@ const Image = ({
                         }
                     }
                     .img:global(.img-loaded) {
-                        animation: loaded 300ms ease-in-out;
+                        animation: loaded 3000ms ease-in-out;
                     }
                 `}
             </style>
             <img
                 ref={imageRef}
-                // width={width}
-                // height={height}
-                style={style}
+                style={{ ...style, ...(!imageSrc && { height: '200px' }) }}
                 className={`img ${className}`}
                 src={
-                    loaded && imageSrc
+                    imageSrc
                         ? `https://thumbor.sirclocdn.xyz/unsafe/${width}x${height}/filters:quality(${quality})/${imageSrc}`
                         : placeholder
                 }
                 alt={alt}
-                onLoad={(event) => {
-                    setLoaded(true);
-                    event.target.classList.add('img-loaded');
+                onLoad={(e) => {
+                    // setLoaded(true);
+                    if (imageSrc) e.target.classList.add('img-loaded');
                 }}
                 onError={(e) => {
-                    setLoaded(true);
+                    // setLoaded(true);
                     e.target.onerror = null;
                     e.target.src = placeholder;
-                }}
-            />
-        </>
-    );
-};
-
-const ThumborImage = ({
-    src, width = 500,
-    height = 500,
-    className = '',
-    alt = 'Image',
-    quality = 100,
-    style = {},
-}) => {
-    const styles = useStyles();
-    const [loaded, setLoaded] = React.useState(typeof window === 'undefined');
-    return (
-        <>
-            <Skeleton variant="rect" width={width} height={height} style={{ display: loaded || typeof window === 'undefined' ? 'none' : 'block' }} />
-            <img
-                style={style}
-                className={!loaded && typeof window !== 'undefined' ? styles.hideImage : className}
-                src={`https://thumbor.sirclocdn.xyz/unsafe/${width}x${height}/filters:quality(${quality})/${src}`}
-                alt={alt}
-                onLoad={() => setLoaded(true)}
-                onError={(e) => {
-                    setLoaded(true);
-                    e.target.onerror = null; e.target.src = '/assets/img/placeholder.png';
                 }}
             />
         </>
