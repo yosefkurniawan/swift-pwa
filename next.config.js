676ee66cb44fb219e09ff257e008855f51362d18
yosefkurniawan <jojo@icube.us>
2020-07-08 07:51:35 +0000

    revert to use experimental as per error on service worker

--- next.config.js
+++ next.config.js
@@ -38,13 +38,15 @@ module.exports = withOffline({
             },
         ],
     },
-    async rewrites() {
-        return [
-            {
-                source: '/service-worker.js',
-                destination: '/_next/static/service-worker.js',
-            },
-        ];
+    experimental: {
+        async rewrites() {
+            return [
+                {
+                    source: '/service-worker.js',
+                    destination: '/_next/static/service-worker.js',
+                },
+            ];
+        },
     },
     // enable code below on Prod and increase the version everytime before running build script
     // generateBuildId: async () => 'swift-pwa-v1.0.0',
