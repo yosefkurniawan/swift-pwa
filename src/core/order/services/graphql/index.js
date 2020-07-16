c72b9da944acdf6ce75fb8e5255bac17111157c3
Asih Priyatno <asih@icube.us>
2020-07-16 12:13:50 +0700

    initial file order core

---
+++ src/core/order/services/graphql/index.js
@@ -0,0 +1,22 @@
+import { useQuery } from '@apollo/react-hooks';
+import * as Schema from './schema';
+
+export const getOrder = (params) => useQuery(Schema.getOrder, {
+    context: {
+        request: 'internal',
+    },
+    variables: params,
+    skip: typeof window === 'undefined',
+});
+
+export const getOrderDetail = (params) => useQuery(Schema.getOrderDetail, {
+    context: {
+        request: 'internal',
+    },
+    variables: params,
+    skip: typeof window === 'undefined',
+});
+
+export default {
+    getOrder,
+};
