07cfa4434283fee51b84d0bbca878271ebec6f09
Asih Priyatno <asih@icube.us>
2020-07-29 13:32:56 +0700

    remove navigation fromcomponents to commons

--- src/layout/index.js
+++ src/layout/index.js
@@ -9,7 +9,7 @@ import Cookies from 'js-cookie';
 import { custDataNameCookie, features } from '@config';
 import { getHost } from '@helpers/config';
 
-const Navigation = dynamic(() => import('@components/Navigation'), { ssr: false });
+const Navigation = dynamic(() => import('@common_navigation'), { ssr: false });
 const Message = dynamic(() => import('@common_toast'), { ssr: false });
 const Loading = dynamic(() => import('@common_loaders/Backdrop'), { ssr: false });
 
