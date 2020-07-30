58861a4e01aa832e005797693db30ef0652c05bf
Asih Priyatno <asih@icube.us>
2020-07-30 11:06:20 +0700

    create forgot password module

--- src/core/forgotpassword/services/graphql/index.js
+++ src/core/forgotpassword/services/graphql/index.js
@@ -1,7 +1,8 @@
-import { useMutation } from '@apollo/react-hooks';
+import { useMutation, useQuery } from '@apollo/react-hooks';
 import * as Schema from './schema';
 
 export const requestLinkToken = () => useMutation(Schema.requestLinkToken);
+export const otpConfig = () => useQuery(Schema.otpConfig);
 
 export default {
     requestLinkToken,
