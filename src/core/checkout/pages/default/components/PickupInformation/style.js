857dcb0e6f2d98a2094dbffddc2c95cf7ca11ab4
yosefkurniawan <jojo@icube.us>
2020-08-05 14:32:50 +0700

    Add missing components for pickup and fix it for desktop view

---
+++ src/core/checkout/pages/default/components/PickupInformation/style.js
@@ -0,0 +1,9 @@
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+export default makeStyles(() => ({
+    card: {
+        marginBottom: 15,
+        marginLeft: 0,
+        marginRight: 0,
+    },
+}));
