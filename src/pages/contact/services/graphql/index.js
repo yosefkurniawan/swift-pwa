94d1dfcbdcb344b729bcb30a3ecc91f068745284
inungbp <inung.bagus@sirclo.com>
2020-08-12 16:03:07 +0700

    delete module contact on src/pages

--- src/pages/contact/services/graphql/index.js
+++
@@ -1,6 +0,0 @@
-import { useMutation } from '@apollo/react-hooks';
-import * as Schema from './schema';
-
-export const contactusFormSubmit = (options) => useMutation(Schema.contactusFormSubmit, { ...options });
-
-export default { contactusFormSubmit };
