1d0b5fdd24c7b1e3b669e24e8f8f531de456633c
rifqi3004 <rifqiattarbani@gmail.com>
2020-08-13 14:11:54 +0700

    fix & clean module contact

--- src/core/contact/services/graphql/index.js
+++ src/core/contact/services/graphql/index.js
@@ -1,6 +1,7 @@
-import { useMutation } from '@apollo/react-hooks';
+import { useMutation, useQuery } from '@apollo/react-hooks';
 import * as Schema from './schema';
 
 export const contactusFormSubmit = (options) => useMutation(Schema.contactusFormSubmit, { ...options });
+export const getCmsBlocks = (variables) => useQuery(Schema.getCmsBlocks, { variables });
 
-export default { contactusFormSubmit };
+export default { contactusFormSubmit, getCmsBlocks };
