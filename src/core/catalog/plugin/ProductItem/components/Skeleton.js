3c19e6c53bbe01e14943768a6e89c12cd306df95
Herlina Sunaryanto <herlina.sunaryanto@sirclo.com>
2020-08-18 11:13:22 +0700

    Product Item Image Skeleton

--- src/core/catalog/plugin/ProductItem/components/Skeleton.js
+++ src/core/catalog/plugin/ProductItem/components/Skeleton.js
@@ -5,7 +5,7 @@ const ProductItemSkeleton = () => {
     const styles = useStyles();
     return (
         <div className={styles.itemContainer}>
-            <Skeleton variant="rect" width="100%" height={200} />
+            <Skeleton variant="rect" width={240} height={300} />
             <Skeleton variant="text" />
             <Skeleton variant="text" />
         </div>
