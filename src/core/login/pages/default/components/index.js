e53b35c3c44a81fca2e9cb17588caeaba489241c
revqiabdillah <rifqi.abdillah@sriclo.com>
2020-08-19 14:57:07 +0700

    update login v2.1

--- src/core/login/pages/default/components/index.js
+++ src/core/login/pages/default/components/index.js
@@ -4,7 +4,9 @@ import Button from '@common_button';
 import Typography from '@common_typography';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
-
+import Divider from '@material-ui/core/Divider';
+import { breakPointsUp } from '@helpers/theme';
+import classNames from 'classnames';
 import OtpBlock from '../../../plugins/otp';
 import OtpView from '../../../plugins/otp/view';
 
@@ -12,85 +14,179 @@ import useStyles from './style';
 
 const Login = (props) => {
     const {
-        formik, otpConfig, isOtp, setIsOtp, t, setDisabled, disabled, loading,
+        formik, otpConfig, isOtp, setIsOtp, t, setDisabled, disabled, loading, formikOtp,
     } = props;
     const styles = useStyles();
+    const desktop = breakPointsUp('sm');
     return (
-        <form onSubmit={formik.handleSubmit} className={styles.container}>
-            {otpConfig.data && otpConfig.data.otpConfig.otp_enable[0].enable_otp_login && (
+        <div className={styles.container}>
+            {!desktop && otpConfig.data && otpConfig.data.otpConfig.otp_enable[0].enable_otp_login && (
                 <FormControlLabel
                     control={<Switch checked={isOtp} onChange={() => setIsOtp(!isOtp)} name="useOtp" color="primary" />}
                     className={styles.selectLogin}
                     label={t('login:switchPhone')}
                 />
             )}
-            {isOtp ? (
-                <OtpBlock
-                    setDisabled={setDisabled}
-                    type="login"
-                    OtpView={OtpView}
-                    phoneProps={{
-                        name: 'username',
-                        placeholder: '+6281234xxxx',
-                        value: formik.values.username,
-                        onChange: formik.handleChange,
-                        error: !!formik.errors.username,
-                        errorMessage: formik.errors.username || null,
-                    }}
-                    codeProps={{
-                        name: 'otp',
-                        value: formik.values.otp,
-                        onChange: formik.handleChange,
-                        error: !!(formik.touched.otp && formik.errors.otp),
-                        errorMessage: (formik.touched.otp && formik.errors.otp) || null,
-                    }}
-                />
-            ) : (
-                <>
-                    <TextField
-                        name="username"
-                        label="Email"
-                        placeholder="john.doe@gmail.com"
-                        value={formik.values.username}
-                        onChange={formik.handleChange}
-                        error={!!formik.errors.username}
-                        errorMessage={formik.errors.username || null}
-                    />
-                    <PasswordField
-                        name="password"
-                        label="Password"
-                        placeholder="********"
-                        value={formik.values.password}
-                        onChange={formik.handleChange}
-                        error={!!formik.errors.password}
-                        errorMessage={formik.errors.password || null}
-                        showVisible
-                    />
-                </>
-            )}
-            <div className={styles.rowCenter}>
-                <Button fullWidth type="submit" disabled={disabled}>
-                    <Typography variant="title" type="regular" letter="capitalize" color="white">
-                        {loading ? 'Loading' : t('login:pageTitle')}
+            <div className={classNames('row between-sm between-md between-lg', styles.desktopContainer)}>
+                <div className="col-sm-12 col-md-12 col-lg-12 hidden-mobile">
+                    <Typography type="bold" variant="h1" className={styles.title}>
+                        Customer Login
                     </Typography>
-                </Button>
-                <Button fullWidth variant="text" href="/customer/account/forgotpassword">
-                    <Typography variant="p" type="regular" letter="capitalize" decoration="underline">
-                        {t('login:forgotPassword')}
-                    </Typography>
-                </Button>
+                </div>
+                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                    <div className="row">
+                        <div className="col-sm-12 hidden-mobile">
+                            <div className={styles.headerSpan}>
+                                <Typography variant="span" className="clear-margin-padding" letter="uppercase">
+                                    Registered Customer
+                                </Typography>
+                            </div>
+                        </div>
+                        {
+                            (!isOtp || desktop) && (
+                                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <div className={classNames(styles.spanLabel, 'hidden-mobile')}>
+                                        <Typography type="bold" variant="p" className="clear-margin-padding">
+                                            If you have an account, sign in with your email address or phone number.
+                                        </Typography>
+                                    </div>
+                                    <form onSubmit={formik.handleSubmit}>
+                                        <div className="row center-xs start-sm">
+                                            <div className="col-xs-12 col-sm-12">
+                                                <TextField
+                                                    name="username"
+                                                    label="Email"
+                                                    placeholder="john.doe@gmail.com"
+                                                    value={formik.values.username}
+                                                    onChange={formik.handleChange}
+                                                    error={!!formik.errors.username}
+                                                    errorMessage={formik.errors.username || null}
+                                                />
+                                            </div>
+                                            <div className="col-xs-12  col-sm-12">
+                                                <PasswordField
+                                                    name="password"
+                                                    label="Password"
+                                                    placeholder="********"
+                                                    value={formik.values.password}
+                                                    onChange={formik.handleChange}
+                                                    error={!!formik.errors.password}
+                                                    errorMessage={formik.errors.password || null}
+                                                    showVisible
+                                                />
+                                            </div>
+                                            <div className="col-xs-12 col-sm-12">
+                                                <Button
+                                                    className={styles.generalButton}
+                                                    fullWidth={!desktop}
+                                                    type="submit"
+                                                    disabled={(desktop) ? false : disabled}
+                                                >
+                                                    <Typography variant="title" type="regular" letter="capitalize" color="white">
+                                                        {loading ? 'Loading' : t('login:pageTitle')}
+                                                    </Typography>
+                                                </Button>
+                                            </div>
+                                            <div className="col-xs-12 col-sm-12">
+                                                <Button fullWidth={false} variant="text" href="/customer/account/forgotpassword">
+                                                    <Typography variant="span" type="regular" letter="capitalize" decoration="underline">
+                                                        {t('login:forgotPassword')}
+                                                    </Typography>
+                                                </Button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            )
+                        }
+                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 hidden-mobile">
+                            <div className={classNames('row middle-sm', styles.spanDivider)}>
+                                <div className="col-sm-12">
+                                    <Divider />
+                                </div>
+                            </div>
+                        </div>
+                        {
+                            (isOtp || desktop) && (
+                                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <div className={classNames(styles.spanLabel, 'hidden-mobile')}>
+                                        <Typography type="bold" variant="p" className="clear-margin-padding">
+                                            If you have an account with registered phone number, sign in with your phone number.
+                                        </Typography>
+                                    </div>
+                                    <form onSubmit={formikOtp.handleSubmit} className={styles.formOtp}>
+                                        <div className="row center-xs start-sm">
+                                            <div className="col-xs-12 col-sm-12">
+                                                <OtpBlock
+                                                    setDisabled={setDisabled}
+                                                    type="login"
+                                                    OtpView={OtpView}
+                                                    phoneProps={{
+                                                        name: 'username',
+                                                        placeholder: '+6281234xxxx',
+                                                        value: formikOtp.values.username,
+                                                        onChange: formikOtp.handleChange,
+                                                        error: !!formikOtp.errors.username,
+                                                        errorMessage: formikOtp.errors.username || null,
+                                                    }}
+                                                    codeProps={{
+                                                        name: 'otp',
+                                                        value: formikOtp.values.otp,
+                                                        onChange: formikOtp.handleChange,
+                                                        error: !!(formikOtp.touched.otp && formikOtp.errors.otp),
+                                                        errorMessage: (formikOtp.touched.otp && formikOtp.errors.otp) || null,
+                                                    }}
+                                                />
+                                            </div>
+                                            <div className="col-xs-12 col-sm-12">
+                                                <Button
+                                                    className={styles.generalButton}
+                                                    fullWidth={!desktop}
+                                                    type="submit"
+                                                    disabled={disabled}
+                                                >
+                                                    <Typography variant="title" type="regular" letter="capitalize" color="white">
+                                                        {loading ? 'Loading' : t('common:button:submit')}
+                                                    </Typography>
+                                                </Button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
+                <div className="col-xs-12 col-sm-5 col-md-5 col-lg-5 hidden-mobile">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <div className={styles.headerSpan}>
+                                <Typography className="clear-margin-padding" variant="span" letter="uppercase">
+                                    New Customer
+                                </Typography>
+                            </div>
+                        </div>
+                        <div className="col-sm-12">
+                            <Typography variant="p">
+                                Creating an account has many benefits: check out faster, keep more than one address, track orders and more.
+                            </Typography>
+                        </div>
+                        <div className="col-sm-12">
+                            <Button
+                                className={styles.generalButton}
+                                fullWidth={false}
+                                href="/customer/account/create"
+                                disabled={(desktop) ? false : disabled}
+                            >
+                                <Typography color="white" variant="title" type="regular" letter="capitalize">
+                                    {t('login:registerTitle')}
+                                </Typography>
+                            </Button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={styles.footer}>
-                <Typography variant="span" letter="capitalize" align="center">
-                    {t('login:notHaveAccount')}
-                </Typography>
-                <Button fullWidth variant="outlined" href="/customer/account/create" disabled={disabled}>
-                    <Typography variant="title" type="regular" letter="capitalize">
-                        {t('login:registerTitle')}
-                    </Typography>
-                </Button>
-            </div>
-        </form>
+        </div>
     );
 };
 
--- src/core/login/pages/default/components/style.js
+++ src/core/login/pages/default/components/style.js
@@ -3,15 +3,29 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 import {
     CreatePadding, FlexColumn, CreateMargin, Centering,
 } from '@theme/mixins';
-import { } from '@theme/colors';
+import { PRIMARY } from '@theme/colors';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
+
     container: {
         height: '100%',
         width: '100%',
         ...FlexColumn,
         alignItems: 'center',
         ...CreatePadding(10, 30, 30, 30),
+        position: 'relative',
+        [theme.breakpoints.down('xs')]: {
+            minHeight: 'calc(100vh - 70px)',
+        },
+    },
+    formOtp: {
+        [theme.breakpoints.down('xs')]: {
+            width: '100vw',
+            ...CreatePadding(0, 30, 30, 30),
+        },
+    },
+    desktopContainer: {
+        marginBottom: 40,
     },
     btnLogin: {
         ...CreateMargin(16, 0, 10, 0),
@@ -34,5 +48,24 @@ export default makeStyles(() => ({
         width: '100%',
         height: '100%',
         textAlign: 'center',
+    },
+    headerSpan: {
+        marginBottom: 20,
+        borderBottom: `1px solid ${PRIMARY}`,
+        paddingBottom: 10,
+    },
+    spanDivider: {
+        height: 120,
+    },
+    spanLabel: {
+        marginBottom: 20,
+    },
+    generalButton: {
+        marginTop: 20,
+    },
+    title: {
+        marginBottom: 30,
+        marginLeft: 0,
+        fontSize: 30,
     },
 }));
