c828d94fcc051dbb34ba7a2a7c7bd38c5230060f
inungbp <inung.bagus@sirclo.com>
2020-08-28 14:11:35 +0700

    brands slick carousel

--- src/core/brands/pages/default/components/featured/index.js
+++ src/core/brands/pages/default/components/featured/index.js
@@ -3,15 +3,19 @@ import Carousel from '@common_slick/Caraousel';
 import ItemFeatured from './Item';
 
 const FeaturedBrands = (props) => {
-    const { featured = [], t, desktop } = props;
+    const {
+        featured = [], t, desktop, logo,
+    } = props;
     return (
-        <Carousel
-            showArrow={desktop}
-            slideLg
-            title={t('brands:featuredBrands')}
-            data={featured}
-            Item={ItemFeatured}
-        />
+        <>
+            <h4 align="center">{t('brands:featuredBrands')}</h4>
+            <Carousel
+                showArrow={desktop}
+                slslideLg={logo ? 4 : 6}
+                data={featured}
+                Item={ItemFeatured}
+            />
+        </>
     );
 };
 
