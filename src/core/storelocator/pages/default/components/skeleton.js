cf99ea4101c15fa8a32dc545a1a48c8ed83f0f21
Bimaa Ghafara <bima@icube.us>
2020-09-10 12:06:00 +0700

    desktop loader

--- src/core/storelocator/pages/default/components/skeleton.js
+++ src/core/storelocator/pages/default/components/skeleton.js
@@ -7,8 +7,8 @@ const SkeletonStoreLocator = () => (
                 <Skeleton variant="rect" width="100%" height={515} />
             </div>
             <div className="col-md-9">
-                <Skeleton variant="rect" width="100%" height={463} />
-                <Skeleton variant="rect" width="100%" height={40} style={{ marginTop: 12 }} />
+                <Skeleton variant="rect" width="100%" height={467} />
+                <Skeleton variant="rect" width="100%" height={36} style={{ marginTop: 12 }} />
             </div>
         </div>
     </>
