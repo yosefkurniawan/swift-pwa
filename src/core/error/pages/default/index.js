27556a70669bfa7e953f1fa8e13f40be5e67be29
Asih Priyatno <asih@icube.us>
2020-09-11 11:22:30 +0700

    fix store config not called on error page

--- src/core/error/pages/default/index.js
+++ src/core/error/pages/default/index.js
@@ -1,10 +1,16 @@
+import { storeConfig as ConfigSchema } from '@services/graphql/schema/config';
 import Core from './core';
 import Content from './components';
+import graphRequest from '../../../../api/graphql/request';
 
 const Error = (props) => <Core {...props} Content={Content} />;
 
-Error.getInitialProps = async () => ({
-    namespacesRequired: ['common'],
-});
+Error.getInitialProps = async () => {
+    const storeConfig = await graphRequest(ConfigSchema);
+    return {
+        namespacesRequired: ['common'],
+        storeConfig,
+    };
+};
 
 export default Error;
