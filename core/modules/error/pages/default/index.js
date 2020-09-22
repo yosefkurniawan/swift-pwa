1706d87db98adbd6fa812bb29ad0526c5971b003
revqiabdillah <revqiabdillah@sirclo.com>
2020-09-22 14:29:26 +0700

    install sentry

--- core/modules/error/pages/default/index.js
+++ core/modules/error/pages/default/index.js
@@ -1,16 +1,38 @@
 import { storeConfig as ConfigSchema } from '@services/graphql/schema/config';
+import * as Sentry from '@sentry/node';
+import NextErrorComponent from 'next/error';
 import Core from './core';
 import Content from './components';
 import graphRequest from '../../../../api/graphql';
 
 const Error = (props) => <Core {...props} Content={Content} />;
 
-Error.getInitialProps = async () => {
+Error.getInitialProps = async ({ res, err, asPath }) => {
+    const errorInitialProps = await NextErrorComponent.getInitialProps({
+        res,
+        err,
+    });
     const storeConfig = await graphRequest(ConfigSchema);
-    return {
-        namespacesRequired: ['common'],
-        storeConfig,
-    };
+
+    errorInitialProps.hasGetInitialPropsRun = true;
+    errorInitialProps.namespacesRequired = ['common'];
+    errorInitialProps.storeConfig = storeConfig;
+
+    if (err) {
+        Sentry.captureException(err);
+        await Sentry.flush(2000);
+        return errorInitialProps;
+    }
+
+    // If this point is reached, getInitialProps was called without any
+    // information about what the error might be. This is unexpected and may
+    // indicate a bug introduced in Next.js, so record it in Sentry
+    Sentry.captureException(
+        new Error(`_error.js getInitialProps missing data at path: ${asPath}`),
+    );
+    await Sentry.flush(2000);
+
+    return errorInitialProps;
 };
 
 export default Error;
