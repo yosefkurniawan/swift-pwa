acc358da4995860574ca316e4dea4fd90e5502c1
Herlina Sunaryanto <herlina.sunaryanto@sirclo.com>
2020-09-29 11:53:37 +0700

    Fixing My Order Page Table

--- core/modules/order/pages/history/style.js
+++ core/modules/order/pages/history/style.js
@@ -77,6 +77,9 @@ export default makeStyles((theme) => ({
         [theme.breakpoints.down('sm')]: {
             border: 'none',
             padding: '8px 0',
+            '&:last-child': {
+                padding: '8px 0',
+            },
         },
     },
     mobLabel: {
