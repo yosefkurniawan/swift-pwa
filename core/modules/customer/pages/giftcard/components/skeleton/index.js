418bd7cd4f7be58144c8f41f020c5ea2e48952a7
Asih Priyatno <asih@icube.us>
2020-10-06 09:31:08 +0700

    fix warning

--- core/modules/customer/pages/giftcard/components/skeleton/index.js
+++ core/modules/customer/pages/giftcard/components/skeleton/index.js
@@ -2,7 +2,7 @@ import Skeleton from '@material-ui/lab/Skeleton';
 import classNames from 'classnames';
 import useStyles from './style';
 
-export default () => {
+const SkeletonLoader = () => {
     const styles = useStyles();
     return (
         <>
@@ -19,3 +19,5 @@ export default () => {
         </>
     );
 };
+
+export default SkeletonLoader;
