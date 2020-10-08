cb37b406dc45e222b89e885f0ae0956de38b200f
Geraldi <geraldiekam@gmail.com>
2020-10-08 17:06:51 +0700

    26434268-styling product list skeleton

--- core/modules/catalog/plugin/ProductItem/components/Skeleton.js
+++ core/modules/catalog/plugin/ProductItem/components/Skeleton.js
@@ -5,7 +5,7 @@ import useStyles from '../style';
 const ProductItemSkeleton = () => {
     const styles = useStyles();
     return (
-        <div className={styles.itemContainer}>
+        <div className={styles.itemContainer} style={{ width: '95%', paddingLeft: 10, paddingRight: 10 }}>
             <Skeleton variant="rect" width={features.imageSize.product.width} height={features.imageSize.product.height} />
             <Skeleton variant="text" width={features.imageSize.product.width} />
             <Skeleton variant="text" width={features.imageSize.product.width} />
