2e7638cf6d7a6158a65938f95d8438fea4aacff8
revqiabdillah <revqiabdillah@sirclo.com>
2020-10-21 15:03:29 +0700

    move title reward point to customer.json

--- core/modules/customer/pages/account/index.js
+++ core/modules/customer/pages/account/index.js
@@ -16,7 +16,7 @@ const Page = (props) => (
 );
 
 Page.getInitialProps = async () => ({
-    namespacesRequired: ['common', 'customer', 'rewardpoint'],
+    namespacesRequired: ['common', 'customer'],
 });
 
 export default withApollo({ ssr: true })(withTranslation()(Page));
