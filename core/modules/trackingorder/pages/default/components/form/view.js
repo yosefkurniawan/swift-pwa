fc622477b614f2c2e1e7cfbf7932b529cfa0283a
revqiabdillah <revqiabdillah@sirclo.com>
2020-10-30 14:26:03 +0700

    fixing layout style

--- core/modules/trackingorder/pages/default/components/form/view.js
+++ core/modules/trackingorder/pages/default/components/form/view.js
@@ -1,46 +1,36 @@
 import Button from '@common_button';
 import TextField from '@common_textfield';
 import Typography from '@common_typography';
+import classNames from 'classnames';
 import useStyles from '../style';
 
 const FormTemplate = (props) => {
-    const {
-        formik, t,
-    } = props;
+    const { formik, t } = props;
     const styles = useStyles();
     return (
         <>
+            <Typography variant="h1" type="bold" letter="uppercase" className={classNames(styles.title, 'hidden-mobile')}>
+                {t('trackingorder:trackingOrder')}
+            </Typography>
             <form className={styles.container} onSubmit={formik.handleSubmit}>
                 <TextField
                     label={t('trackingtrackingorder:email')}
                     name="email"
                     value={formik.values.email}
                     onChange={formik.handleChange}
-                    error={
-                        !!(formik.touched.email && formik.errors.email)
-                    }
-                    errorMessage={
-                        (formik.touched.email && formik.errors.email)
-                    || null
-                    }
+                    error={!!(formik.touched.email && formik.errors.email)}
+                    errorMessage={(formik.touched.email && formik.errors.email) || null}
                 />
                 <TextField
                     label={t('trackingorder:orderId')}
                     name="order_id"
                     value={formik.values.order_id}
                     onChange={formik.handleChange}
-                    error={
-                        !!(formik.touched.order_id && formik.errors.order_id)
-                    }
-                    errorMessage={
-                        (formik.touched.order_id && formik.errors.order_id) || null
-                    }
+                    error={!!(formik.touched.order_id && formik.errors.order_id)}
+                    errorMessage={(formik.touched.order_id && formik.errors.order_id) || null}
                 />
                 <div className={styles.bottomButtons}>
-                    <Button
-                        fullWidth
-                        type="submit"
-                    >
+                    <Button fullWidth type="submit" rootClassName={styles.btnSeacrh}>
                         <Typography variant="span" type="bold" letter="uppercase" color="white">
                             {t('common:search:title')}
                         </Typography>
