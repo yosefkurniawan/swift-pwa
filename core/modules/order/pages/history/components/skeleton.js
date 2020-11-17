b2d947f50ffe3071422e549dd900739c80247997
mathiusaldi <geraldiekam@gmail.com>
2020-11-17 14:58:01 +0700

    26501100-change layout skeleton

--- core/modules/order/pages/history/components/skeleton.js
+++ core/modules/order/pages/history/components/skeleton.js
@@ -2,47 +2,41 @@ import Skeleton from '@material-ui/lab/Skeleton';
 import classNames from 'classnames';
 import useStyles from '../style';
 
-const data = [1, 2, 3];
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export const SkeletonContent = () => {
     const styles = useStyles();
     return (
         <>
             <div className="hidden-desktop">
-                {
-                    data.map((i) => (
-                        <div key={i} className={styles.itemContainer}>
-                            <div className={styles.contentItem}>
-                                <Skeleton variant="text" width={70} height={15} animation="wave" />
-                                <Skeleton variant="text" width={120} height={20} animation="wave" />
-                                <div className={styles.detailItem}>
-                                    <div className={`column ${styles.columnLabel}`}>
-                                        <Skeleton variant="text" width="80%" height={15} animation="wave" />
-                                        <Skeleton variant="text" width="80%" height={15} animation="wave" />
-                                        <Skeleton variant="text" width="80%" height={15} animation="wave" />
-                                    </div>
-                                    <div className={styles.detailContent}>
-                                        <Skeleton variant="text" width="80%" height={15} animation="wave" />
-                                        <Skeleton variant="text" width="80%" height={15} animation="wave" />
-                                        <Skeleton variant="text" width="80%" height={15} animation="wave" />
-                                    </div>
+                {data.map((i) => (
+                    <div key={i} className={styles.itemContainer}>
+                        <div className={styles.contentItem}>
+                            <Skeleton variant="text" width={70} height={15} animation="wave" />
+                            <Skeleton variant="text" width={120} height={20} animation="wave" />
+                            <div className={styles.detailItem}>
+                                <div className={`column ${styles.columnLabel}`}>
+                                    <Skeleton variant="text" width="80%" height={15} animation="wave" />
+                                    <Skeleton variant="text" width="80%" height={15} animation="wave" />
+                                    <Skeleton variant="text" width="80%" height={15} animation="wave" />
+                                </div>
+                                <div className={styles.detailContent}>
+                                    <Skeleton variant="text" width="80%" height={15} animation="wave" />
+                                    <Skeleton variant="text" width="80%" height={15} animation="wave" />
+                                    <Skeleton variant="text" width="80%" height={15} animation="wave" />
                                 </div>
                             </div>
                         </div>
-                    ))
-                }
+                    </div>
+                ))}
             </div>
             <div className="hidden-mobile">
-                {
-                    data.map((i) => (
-                        <Skeleton key={i} variant="text" width="100%" height={30} animation="wave" />
-                    ))
-                }
-                {
-                    data.map((i) => (
-                        <Skeleton key={i} variant="text" width="100%" height={30} animation="wave" />
-                    ))
-                }
+                {data.map((i) => (
+                    <Skeleton key={i} variant="text" width="100%" height={30} animation="wave" />
+                ))}
+                {data.map((i) => (
+                    <Skeleton key={i} variant="text" width="100%" height={30} animation="wave" />
+                ))}
             </div>
         </>
     );
@@ -52,30 +46,34 @@ const SkeletonLoader = () => {
     const styles = useStyles();
     return (
         <>
-            <div className="row hidden-mobile">
-                <div className="col-md-10">
-                    <div className={styles.container}>
-                        {data.map((i) => (
-                            <div key={i} className={styles.itemContainer}>
-                                <div className={styles.contentItem}>
-                                    <Skeleton variant="text" width={70} height={15} animation="wave" />
-                                    <Skeleton variant="text" width={120} height={20} animation="wave" />
-                                    <div className={styles.detailItem}>
-                                        <div className={`column ${styles.columnLabel}`}>
-                                            <Skeleton variant="text" width="80%" height={15} animation="wave" />
-                                            <Skeleton variant="text" width="80%" height={15} animation="wave" />
-                                            <Skeleton variant="text" width="80%" height={15} animation="wave" />
-                                        </div>
-                                        <div className={styles.detailContent}>
-                                            <Skeleton variant="text" width="80%" height={15} animation="wave" />
-                                            <Skeleton variant="text" width="80%" height={15} animation="wave" />
-                                            <Skeleton variant="text" width="80%" height={15} animation="wave" />
-                                        </div>
+            <div className="hidden-mobile">
+                <div className={styles.container}>
+                    {data.map((i) => (
+                        <div key={i} className={styles.itemContainer}>
+                            <div className={styles.contentItem}>
+                                <div className={styles.detailItem}>
+                                    <div className={`column ${styles.columnLabelId}`}>
+                                        <Skeleton variant="text" width="80%" height={15} animation="wave" />
+                                    </div>
+                                    <div className={`column ${styles.columnLabelDate}`}>
+                                        <Skeleton variant="text" width="80%" height={15} animation="wave" />
+                                    </div>
+                                    <div className={`column ${styles.columnLabelShipped}`}>
+                                        <Skeleton variant="text" width="80%" height={15} animation="wave" />
+                                    </div>
+                                    <div className={`column ${styles.columnLabelTotal}`}>
+                                        <Skeleton variant="text" width="80%" height={15} animation="wave" />
+                                    </div>
+                                    <div className={`column ${styles.columnLabelStatus}`}>
+                                        <Skeleton variant="text" width="80%" height={15} animation="wave" />
+                                    </div>
+                                    <div className={`column ${styles.columnLabelAction}`}>
+                                        <Skeleton variant="text" width="80%" height={15} animation="wave" />
                                     </div>
                                 </div>
                             </div>
-                        ))}
-                    </div>
+                        </div>
+                    ))}
                 </div>
             </div>
             <div className={classNames(styles.container, 'hidden-desktop')}>
