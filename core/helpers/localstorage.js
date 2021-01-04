05b1d87d8e7ee9624cd173a6b7e6bae6157af360
revqiabdillah <rifqi.abdillah@sirclo.com>
2021-01-04 14:43:22 +0700

    fix([26524032] Sentry - cookies): add conditional get & set cookies

--- core/helpers/localstorage.js
+++ core/helpers/localstorage.js
@@ -1,7 +1,18 @@
 import { localResolverKey } from '@config';
 
+const testCookies = () => {
+    const test = 'test';
+    try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
 export const setLocalStorage = (key, data) => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined' && testCookies() === true) {
         let localData = data;
         if (data && data !== null) {
             localData = JSON.stringify(data);
@@ -12,7 +23,7 @@ export const setLocalStorage = (key, data) => {
 };
 
 export const getLocalStorage = (key) => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined' && testCookies() === true) {
         const data = localStorage.getItem(key);
         if (data && data !== null) {
             return JSON.parse(data);
