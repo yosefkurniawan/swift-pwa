ec53775ca8b343f789c116718095f85a94113e8b
Abinowo <abinowo.ananta@gmail.com>
2021-03-31 14:54:25 +0700

    fix([26681513] Newsletter): cleaning code and fix alert show

--- core/modules/customer/pages/newsletter/core.js
+++ core/modules/customer/pages/newsletter/core.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-return-assign */
+import { useRef, useEffect, useState } from 'react';
 import Layout from '@layout';
 import { useMutation } from '@apollo/client';
 import { getCustomerSettings } from '../../services/graphql';
@@ -8,6 +10,15 @@ const NewsletterPage = (props) => {
     const {
         t, Content, pageConfig, app_cookies,
     } = props;
+    const [actUpdateCustomer, { data: dataUpdate, loading }] = useMutation(Schema.updateCustomer);
+    const { data } = getCustomerSettings();
+
+    const [customer, setCustomer] = useState({});
+    const [settings, setSettings] = useState({
+        is_subscribed: false,
+    });
+
+    const mount = useRef();
     const config = {
         title: t('customer:setting:newsletter'),
         header: 'relative', // available values: "absolute", "relative", false (default)
@@ -15,23 +26,48 @@ const NewsletterPage = (props) => {
         bottomNav: false,
     };
 
-    const [settings, setSettings] = React.useState({
-        is_subscribed: false,
-    });
+    /**
+     * [useEffect] lifecycle react
+     * @check []
+     */
+    useEffect(() => {
+        mount.current = true;
+        if (mount.current) {
+            if (typeof window !== 'undefined') {
+                if (data && data.customer) {
+                    const customerData = data.customer;
+                    const { is_subscribed } = customerData;
+                    const settingConfig = { is_subscribed };
+                    setCustomer(customerData);
+                    setSettings({
+                        ...settingConfig,
+                    });
+                }
+            }
+        }
+        return () => (mount.current = false);
+    }, [data]);
 
-    const [actUpdateCustomer, resultUpdate] = useMutation(Schema.updateCustomer);
+    /**
+     * [useEffect] check
+     * @check [loading]
+     */
+    useEffect(() => {
+        if (mount.current && !loading && dataUpdate !== undefined) {
+            setTimeout(() => {
+                window.backdropLoader(false);
+                window.toastMessage({
+                    open: true,
+                    text: t('customer:setting:newsletter_success'),
+                    variant: 'success',
+                });
+            }, 500);
+        }
+    }, [loading]);
 
-    if (!resultUpdate.loading && typeof window !== 'undefined' && window.backdropLoader) {
-        setTimeout(() => {
-            window.backdropLoader(false);
-            window.toastMessage({
-                open: true,
-                text: t('customer:setting:newsletter_success'),
-                variant: 'success',
-            });
-        }, 500);
-    }
-
+    /**
+     * [METHOD] handle save button
+     */
     const handleSave = async () => {
         window.backdropLoader(true);
         try {
@@ -43,32 +79,16 @@ const NewsletterPage = (props) => {
                     request: 'internal',
                 },
             });
+            window.backdropLoader(false);
         } catch (e) {
             await window.toastMessage({
                 open: true,
                 text: e.message.split(':')[1] || t('customer:setting:newsletter_error'),
                 variant: 'error',
             });
+            window.backdropLoader(false);
         }
     };
-
-    let customer = {};
-    if (typeof window !== 'undefined') {
-        const { data } = getCustomerSettings();
-        if (data && data.customer) {
-            customer = data.customer;
-        }
-    }
-
-    React.useEffect(() => {
-        if (customer.is_subscribed) {
-            setSettings({
-                ...{
-                    is_subscribed: true,
-                },
-            });
-        }
-    }, [customer]);
 
     return (
         <Layout {...props} pageConfig={pageConfig || config}>
