5eeeb0e0b1e2640d093902484cf8ba8c10ce06e1
Abinowo <abinowo.ananta@gmail.com>
2021-04-07 11:30:59 +0700

    feat([26681517] Product List): add product list widget

--- core/modules/cms/components/widget/list-product/index.js
+++ core/modules/cms/components/widget/list-product/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const WidgetListProduct = () => <div>WidgetListProduct</div>;
+const WidgetListProduct = (props) => {
+    const { conditions_encoded } = props;
+    const condition_object = conditions_encoded.split('/');
+    console.log('condition_object', condition_object);
+    return <div>WidgetListProduct</div>;
+};
 
 export default WidgetListProduct;
