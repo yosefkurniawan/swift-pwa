a62652cf6630fe3452e6cce59c278e81ee16e7a8
Yosef Kurniawan <jojo@icube.us>
2021-04-19 17:12:53 +0700

    chore([26696513] Homepage - CMS): Disable CMS Homepage mode as default for now

--- swift.config.js
+++ swift.config.js
@@ -420,7 +420,7 @@ const modules = {
     home: {
         enabled: true,
         useCmsPage: {
-            enable: true,
+            enable: false,
             identifier: 'pwa-homepage',
         },
         featuresProduct: {
