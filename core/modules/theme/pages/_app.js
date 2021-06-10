45e46ae66412964a6f2bf841a3255d82ce80cd0e
Yosef Kurniawan <jojo@icube.us>
2021-06-10 08:52:38 +0700

    feat([26770213] Maintenance): Adding comment

--- core/modules/theme/pages/_app.js
+++ core/modules/theme/pages/_app.js
@@ -127,6 +127,9 @@ class MyApp extends App {
         if (!pageProps.storeConfig && (!storeConfig || typeof window === 'undefined')) {
             // storeConfig = await apolloClient.query({ query: ConfigSchema }).then(({ data }) => data.storeConfig);
             storeConfig = await graphRequest(ConfigSchema);
+
+            // Handle redirecting to tomaintenance page automatically when GQL is in maintenance mode.
+            // We do this here since query storeConfig is the first query and be done in server side
             if (ctx && storeConfig.response && storeConfig.response.status && storeConfig.response.status > 500) {
                 ctx.res.redirect('/maintenance');
             }
