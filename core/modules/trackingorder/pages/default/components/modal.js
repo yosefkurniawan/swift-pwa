d6e5d92bd1b4bfb466de14b66518fd7718ce76cb
inungbp <inung.bagus@sirclo.com>
2021-06-29 14:48:21 +0700

    feat([26811777] customer - tracking): Support Live Tracking Anteraja, JNE, and SAP

--- core/modules/trackingorder/pages/default/components/modal.js
+++ core/modules/trackingorder/pages/default/components/modal.js
@@ -17,6 +17,9 @@ import Link from 'next/link';
 
 import { checkJson } from '@core_modules/trackingorder/pages/default/helpers/checkJson';
 import useStyles from '@core_modules/trackingorder/pages/default/components/style';
+import ShipperJNE from '@core_modules/trackingorder/pages/default/components/shipper/jne';
+import ShipperAnterAja from '@core_modules/trackingorder/pages/default/components/shipper/anterAja';
+import ShipperSAP from '@core_modules/trackingorder/pages/default/components/shipper/sap';
 
 const Transition = React.forwardRef((props, ref) => <Slide direction="up" ref={ref} {...props} />);
 
@@ -35,6 +38,9 @@ const ModalResult = (props) => {
             let { detail } = data;
             [detail] = detail;
             const items = [];
+            const anterAja = [];
+            const jneItems = [];
+            const sapItems = [];
             const gosend = detail.shipping_methods.shipping_description.match(/go-send/i);
             if (detail.shipping_methods.shipping_detail[0].data_detail) {
                 let dt = detail.shipping_methods.shipping_detail[0].data_detail;
@@ -43,29 +49,59 @@ const ModalResult = (props) => {
                 dt = dt.replace(/`/g, '"');
                 if (checkJson(dt) && !JSON.parse(dt).errors) {
                     dt = JSON.parse(dt);
-                    const keys = Object.keys(dt);
                     const listField = gosend ? trackingorder.fieldDetail.gosend : trackingorder.fieldDetail.shipperid;
-                    for (let idx = 0; idx < keys.length; idx += 1) {
-                        if (listField.includes(keys[idx])) {
-                            let secondary = dt[keys[idx]];
-                            if (secondary !== null && secondary !== '' && secondary.includes('http')) {
-                                secondary = (
-                                    <Link href={secondary}>
-                                        <a target="_blank" className="item-link">
-                                            {secondary}
-                                        </a>
-                                    </Link>
-                                );
-                            }
+                    if (dt.content) {
+                        const nameReceiver = dt.content.order.receiver.name;
+                        const description = dt.content.history[0].message.id;
+                        const updateDate = dt.content.history[0].timestamp;
+                        anterAja.push({
+                            nameReceiver,
+                            description,
+                            updateDate,
+                        });
+                    } if (dt.cnote) {
+                        const nameReceiver = dt.cnote.cnote_receiver_name;
+                        const description = dt.cnote.keterangan;
+                        const updateDate = dt.cnote.cnote_date;
+                        jneItems.push({
+                            nameReceiver,
+                            description,
+                            updateDate,
+                        });
+                    } if (dt && dt[0] && dt[0].counter_name) {
+                        const sap = dt[dt.length - 1];
+                        const nameReceiver = sap.receiver_name;
+                        const descriptionSap = sap.description;
+                        const updateDate = sap.create_date;
+                        sapItems.push({
+                            nameReceiver,
+                            descriptionSap,
+                            updateDate,
+                        });
+                    } else {
+                        const keys = Object.keys(dt);
+                        for (let idx = 0; idx < keys.length; idx += 1) {
+                            if (listField.includes(keys[idx])) {
+                                let secondary = dt[keys[idx]];
+                                if (secondary !== null && secondary !== '' && secondary.includes('http')) {
+                                    secondary = (
+                                        <Link href={secondary}>
+                                            <a target="_blank" className="item-link">
+                                                {secondary}
+                                            </a>
+                                        </Link>
+                                    );
+                                }
 
-                            if (secondary !== null && secondary.length <= 30) {
-                                const date = formatDate(secondary);
-                                if (date !== 'Invalid Date') secondary = date;
+                                if (secondary !== null && secondary.length <= 30) {
+                                    const date = formatDate(secondary);
+                                    if (date !== 'Invalid Date') secondary = date;
+                                }
+                                items.push({
+                                    primary: startCase(keys[idx]),
+                                    secondary,
+                                });
                             }
-                            items.push({
-                                primary: startCase(keys[idx]),
-                                secondary,
-                            });
                         }
                     }
                 } else {
@@ -79,6 +115,41 @@ const ModalResult = (props) => {
                 <div className="row">
                     <div className="col-xs-12">
                         <List>
+                            {
+                                anterAja.length !== 0
+                                    ? (
+                                        <ShipperAnterAja
+                                            nameReceiver={anterAja[0].nameReceiver}
+                                            description={anterAja[0].description}
+                                            updateDate={anterAja[0].updateDate}
+                                            styles={styles}
+                                        />
+                                    ) : null
+                            }
+                            {
+                                jneItems.length !== 0
+                                    ? (
+                                        <ShipperJNE
+                                            nameReceiver={jneItems[0].nameReceiver}
+                                            description={jneItems[0].description}
+                                            updateDate={jneItems[0].updateDate}
+                                            styles={styles}
+                                        />
+                                    )
+                                    : null
+                            }
+                            {
+                                sapItems.length !== 0
+                                    ? (
+                                        <ShipperSAP
+                                            nameReceiver={sapItems[0].nameReceiver}
+                                            description={sapItems[0].descriptionSap}
+                                            updateDate={sapItems[0].updateDate}
+                                            styles={styles}
+                                        />
+                                    )
+                                    : null
+                            }
                             {items.map((item, i) => (
                                 <ListItem key={i} className={styles.listItem}>
                                     <ListItemText
---
+++ core/modules/trackingorder/pages/default/components/shipper/sap.js
@@ -0,0 +1,23 @@
+const ShipperSAP = (props) => {
+    const {
+        nameReceiver, description, updateDate, styles,
+    } = props;
+    return (
+        <div className={styles.containerList}>
+            <div className="list-item">
+                <p className="list-item__title">Name</p>
+                <p className="list-item__desc">{nameReceiver}</p>
+            </div>
+            <div className="list-item">
+                <p className="list-item__title">Description</p>
+                <p className="list-item__desc">{description}</p>
+            </div>
+            <div className="list-item">
+                <p className="list-item__title">updateDate</p>
+                <p className="list-item__desc">{updateDate}</p>
+            </div>
+        </div>
+    );
+};
+
+export default ShipperSAP;
