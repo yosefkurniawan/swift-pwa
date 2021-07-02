9cc79163445bbb96d5c0dcd1752b0fe2d36891e9
sureyeahdye <suryadi.darmawan@sirclo.com>
2021-07-02 14:13:48 +0700

    feat([26681513] Multi Language Switcher): adjust

--- core/modules/slug/services/graphql/index.js
+++ core/modules/slug/services/graphql/index.js
@@ -1,6 +1,8 @@
 import { useQuery } from '@apollo/client';
 import * as Schema from '@core_modules/slug/services/graphql/schema';
 
-export const getResolver = (urlpath) => useQuery(Schema.getResolver(urlpath));
+export const getResolver = (urlpath) => useQuery(Schema.getResolver(urlpath), {
+    fetchPolicy: 'cache-and-network',
+});
 
 export default { getResolver };
