333aaf2c858b902887b527362c1a18ec7145348d
widoicube <wido@icube.us>
2021-07-14 00:34:55 +0700

    feat([26844544] Add icon magezone cms): Add icon magezone cms

--- core/modules/cms/components/cms-renderer/magezon/MagezonButton.js
+++ core/modules/cms/components/cms-renderer/magezon/MagezonButton.js
@@ -2,10 +2,11 @@
 import React from 'react';
 import Button from '@common_button';
 import MagezonLink from '@core_modules/cms/components/cms-renderer/magezon/MagezonLink';
+import MagezonIcon from '@core_modules/cms/components/cms-renderer/magezon/MagezoneIcon';
 
 const MagezonButton = (props) => {
     const {
-        xs_hide, sm_hide, md_hide, lg_hide, title, link,
+        xs_hide, sm_hide, button_size, md_hide, lg_hide, title, link, icon,
     } = props;
     let classes = '';
     if (xs_hide) classes += 'hidden-mobile ';
@@ -14,26 +15,21 @@ const MagezonButton = (props) => {
     if (lg_hide) classes += 'hidden-lg ';
     return (
         <div className={classes}>
-            {
-                link && link !== ''
-                    ? (
-                        <MagezonLink link={link}>
-
-                            <Button className="magezon-button" type="button" align="left">
-                                {title || ''}
-                            </Button>
-                        </MagezonLink>
-                    )
-                    : (
-
-                        <Button className="magezon-button" type="button" align="left">
-                            {title || ''}
-                        </Button>
-                    )
-            }
+            {link && link !== '' ? (
+                <MagezonLink link={link}>
+                    <Button className="magezon-button" type="button" align="left">
+                        {icon !== '' ? <MagezonIcon icon={icon} icon_size={button_size} /> : null}
+                        {title || ''}
+                    </Button>
+                </MagezonLink>
+            ) : (
+                <Button className="magezon-button" type="button" align="left">
+                    {icon !== '' ? <MagezonIcon icon={icon} icon_size={button_size} /> : null}
+                    {title || ''}
+                </Button>
+            )}
             <style jsx>
                 {`
-                    
                 `}
             </style>
         </div>
