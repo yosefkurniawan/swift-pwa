d88a65c6cf191919ad69c234d280bf3b6d4e17fa
sureyeahdie <sureyeahdie@gmail.com>
2021-07-13 16:10:04 +0700

    feat([26844559] Magezon Social - Twitter Timeline)

--- core/modules/cms/components/cms-renderer/magezon/MagezonTwitterTimeline/index.js
+++ core/modules/cms/components/cms-renderer/magezon/MagezonTwitterTimeline/index.js
@@ -1,32 +1,7 @@
-import { useEffect } from 'react';
-
 const TwitterTimeline = (props) => {
     const {
-        box_height, box_width, limit, theme, page_url, chrome, show_replies,
+        box_height, box_width, limit, theme, page_url, chrome, show_replies, t,
     } = props;
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            /* eslint-disable */
-            window.twttr = (function (d, s, id) {
-                let js,
-                    fjs = d.getElementsByTagName(s)[0],
-                    t = window.twttr || {};
-                if (d.getElementById(id)) return t;
-                js = d.createElement(s);
-                js.id = id;
-                js.src = 'https://platform.twitter.com/widgets.js';
-                fjs.parentNode.insertBefore(js, fjs);
-
-                t._e = [];
-                t.ready = function (f) {
-                    t._e.push(f);
-                };
-
-                return t;
-            })(document, 'script', 'twitter-wjs');
-        }
-    }, []);
 
     return (
         <>
@@ -40,8 +15,7 @@ const TwitterTimeline = (props) => {
                 show-replies={show_replies}
                 href={`https://twitter.com/${page_url}?ref_src=twsrc%5Etfw`}
             >
-                Tweets by TwitterDev
-                {/* ${} */}
+                {`${t('common:twitter:tweetBy')} ${page_url}`}
             </a>
         </>
     );
