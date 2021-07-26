fa1d2cd80e93d4e8c1ae23c0b98aa79ddb0105ca
widoicube <wido@icube.us>
2021-07-26 16:13:31 +0700

    feat([26844563] Add contact us magezon): Add contact us magezon

---
+++ core/modules/cms/components/cms-renderer/magezon/MagezonContactForm.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ContactForm from '@core_modules/contact/pages/default/index';
+
+const MagezonContactForm = () => <ContactForm isCms />;
+
+export default MagezonContactForm;
--- core/modules/cms/components/cms-renderer/magezon/index.js
+++ core/modules/cms/components/cms-renderer/magezon/index.js
@@ -13,6 +13,7 @@ import MagezonEmpty from '@core_modules/cms/components/cms-renderer/magezon/Mage
 import MagezonFanspage from '@core_modules/cms/components/cms-renderer/magezon/MagezonFanspage';
 import MagezonToggle from '@core_modules/cms/components/cms-renderer/magezon/MagezonToggle';
 import MagezonMessageBox from '@core_modules/cms/components/cms-renderer/magezon/MagezonMessageBox';
+import MagezonContactForm from '@core_modules/cms/components/cms-renderer/magezon/MagezonContactForm';
 import generateCustomCssAnimation from '@core_modules/cms/helpers/magezonCustomCssAnimationGenerator';
 import dynamic from 'next/dynamic';
 import 'font-awesome/css/font-awesome.min.css';
@@ -130,6 +131,8 @@ const MagezonElement = (props) => {
             childrenContent = <MagezonToggle {...props} />; break;
         case 'message_box':
             childrenContent = <MagezonMessageBox {...props} />; break;
+        case 'contact_form':
+            childrenContent = <MagezonContactForm {...props} />; break;
         default:
             childrenContent = null;
         }
