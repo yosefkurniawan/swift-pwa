f6f5bc0ac6e9a120a0c17c57e2c287ac5512c814
Peter Santoso <peter.santoso@sirclo.com>
2021-07-29 15:28:08 +0700

    feat([26844562] Add search form magezon): fixing style based on feedback

--- core/modules/cms/components/cms-renderer/magezon/MagezonSearchForm.js
+++ core/modules/cms/components/cms-renderer/magezon/MagezonSearchForm.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Autocomplete from '@core_modules/theme/components/header/desktop/components/autocomplete';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
+import TextField from '@material-ui/core/TextField';
 import OptionAutocomplete from '@core_modules/theme/components/header/desktop/components/autocomplete/view';
 import Router from 'next/router';
 import { useTranslation } from '@i18n';
 
-const MagezonSearchForm = () => {
+const MagezonSearchForm = (props) => {
+    const { placeholder, form_width, input_background_color, input_text_color } = props;
     const [value, setValue] = React.useState('');
     const { t } = useTranslation();
     const handleSearch = (ev) => {
@@ -17,6 +19,7 @@ const MagezonSearchForm = () => {
             });
         }
     };
+    console.log(props);
 
     const searchByClick = () => {
         if (value !== '') {
@@ -26,28 +29,39 @@ const MagezonSearchForm = () => {
             });
         }
     };
+
     return (
         <div className="header-middle__search">
-            <Autocomplete setValue={setValue} handleSearch={handleSearch} OptionsItem={OptionAutocomplete} t={t} />
+            <Autocomplete 
+                setValue={setValue} 
+                handleSearch={handleSearch} 
+                OptionsItem={OptionAutocomplete} 
+                t={t} 
+                width={parseInt(form_width)} 
+                forcePopupIcon={false} 
+                popupIcon={<SearchIcon />} 
+                placeholder={placeholder}
+            />
             <div className="search-icon">
-                <IconButton disabled={value === ''} edge="start" onClick={searchByClick} aria-label="close">
+                <IconButton disabled={value === ''} edge="start" onClick={searchByClick} aria-label="search">
                     <SearchIcon />
                 </IconButton>
             </div>
-            <style global jsx>
+            <style jsx>
                 {`
                     .search-icon {
                         position: absolute;
                         right: -10px;
-                        top: 7px;
-                        background: #fff;
-                        z-index: 9;
+                        top: 0px;
+                        background: #ffffff00;
                     }
                     .header-middle__search {
                         display: flex;
                         align-items: center;
                         float: left;
                         position: relative;
+                        background-color: ${input_background_color};
+                        color: ${input_text_color};
                     }
                 `}
             </style>
--- core/modules/theme/components/header/desktop/components/autocomplete/index.js
+++ core/modules/theme/components/header/desktop/components/autocomplete/index.js
@@ -38,7 +38,7 @@ const generateItemData = (product, category) => {
 
 export default function ComboBox(props) {
     const {
-        handleSearch, setValue, OptionsItem, width = 300, maxHeight = '80vh', t,
+        placeholder, handleSearch, setValue, OptionsItem, forcePopupIcon = true,  width = 300, maxHeight = '80vh', t,
     } = props;
     const [item, setItem] = React.useState([]);
     const [open, setOpen] = React.useState(false);
@@ -97,8 +97,8 @@ export default function ComboBox(props) {
             options={item}
             getOptionLabel={(option) => option.name}
             getOptionSelected={(option, value) => option.name === value.name}
-            forcePopupIcon
-            style={{ width }}
+            forcePopupIcon={forcePopupIcon}
+            style={{ width, padding: '2px 5px 5px 5px' }}
             openOnFocus={false}
             open={open}
             ListboxProps={{ style: { maxHeight, height: 'auto' } }}
@@ -106,7 +106,7 @@ export default function ComboBox(props) {
             renderInput={(params) => (
                 <TextField
                     id="standard-basic"
-                    label={t('common:title:search')}
+                    label={placeholder || t('common:title:search')}
                     margin="normal"
                     onBlur={() => {
                         setClose(true);
