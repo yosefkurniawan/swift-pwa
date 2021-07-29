d8855a275e70a5a1cd199a4fb8654092f77f099f
sureyeahdie <sureyeahdie@gmail.com>
2021-07-29 12:20:39 +0700

    bug([26885981] Magezon - Global Style)

--- core/modules/cms/components/cms-renderer/magezon/MagezonColumn.js
+++ core/modules/cms/components/cms-renderer/magezon/MagezonColumn.js
@@ -4,26 +4,26 @@ import useStyles from '@core_modules/cms/components/cms-renderer/magezon/style';
 
 const MagezonColumn = (props) => {
     const {
-        elements, xs_size, sm_size, md_size, lg_size,
-        xs_offset_size, sm_offset_size, md_offset_size, lg_offset_size,
-        xs_hide, sm_hide, md_hide, lg_hide, storeConfig,
+        elements, xs_size, sm_size, md_size, lg_size, xl_size,
+        xs_offset_size, sm_offset_size, md_offset_size, lg_offset_size, xl_offset_size,
+        xs_hide, sm_hide, md_hide, lg_hide, xl_hide, storeConfig,
     } = props;
     const classes = useStyles(props);
     let classColumn = 'mgz-column ';
     if (xs_size && xs_size !== '') classColumn += `col-xs-${xs_size} `;
     if (sm_size && sm_size !== '') classColumn += `col-sm-${sm_size} `;
     if (md_size && md_size !== '') classColumn += `col-md-${md_size} `;
-    if (lg_size && lg_size !== '') classColumn += `col-lg-${lg_size} `;
+    if ((lg_size && lg_size !== '') || (xl_size && xl_size !== '')) classColumn += `col-lg-${lg_size} `;
 
     if (xs_offset_size && xs_offset_size !== '') classColumn += `col-xs-offset-${xs_offset_size} `;
     if (sm_offset_size && sm_offset_size !== '') classColumn += `col-sm-offset-${sm_offset_size} `;
     if (md_offset_size && md_offset_size !== '') classColumn += `col-md-offset-${md_offset_size} `;
-    if (lg_offset_size && lg_offset_size !== '') classColumn += `col-lg-offset-${lg_offset_size} `;
+    if ((lg_offset_size && lg_offset_size !== '') || (xl_offset_size && xl_offset_size !== '')) classColumn += `col-lg-offset-${lg_offset_size} `;
 
     if (xs_hide) classColumn += 'hidden-mobile ';
     if (sm_hide) classColumn += 'hidden-sm ';
     if (md_hide) classColumn += 'hidden-md ';
-    if (lg_hide) classColumn += 'hidden-lg ';
+    if (lg_hide || xl_hide) classColumn += 'hidden-lg ';
 
     if (!classColumn.includes('col-')) {
         classColumn += 'col-xs-12 col-lg-12';
