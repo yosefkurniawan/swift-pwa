7072b70309d914a8f51ac6b13acd0e7bf844d511
sureyeahdie <sureyeahdie@gmail.com>
2021-08-18 12:36:24 +0700

    fix([26894601]): adjust breakpoints

--- core/modules/cms/components/cms-renderer/magezon/index.js
+++ core/modules/cms/components/cms-renderer/magezon/index.js
@@ -25,6 +25,7 @@ import dynamic from 'next/dynamic';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'open-iconic/font/css/open-iconic-bootstrap.css';
 import 'animate.css';
+import useStyles from '@core_modules/cms/components/cms-renderer/magezon/style';
 
 const MagezonInstagram = dynamic(() => import('@core_modules/cms/components/cms-renderer/magezon/MagezonInstagramFeed'), { ssr: false });
 const MagezonPinterest = dynamic(() => import('@core_modules/cms/components/cms-renderer/magezon/MagezonPinterest'), { ssr: false });
@@ -45,8 +46,9 @@ const MagezonElement = (props) => {
         storeConfig,
     } = props;
     const { base_media_url } = storeConfig;
+    const customStyles = useStyles();
     let childrenContent;
-    let classes = 'mgz-element ';
+    let classes = `${customStyles.wrapper} mgz-element `;
     const { className, styles } = generateCustomCssAnimation(animation_duration, animation_delay, animation_infinite);
 
     const enumCustomAnimation = {
@@ -66,11 +68,11 @@ const MagezonElement = (props) => {
 
     if (disable_element) return null;
 
-    if (xs_hide) classes += 'hidden-mobile ';
-    if (sm_hide) classes += 'hidden-sm ';
-    if (md_hide) classes += 'hidden-md ';
-    if (lg_hide) classes += 'hidden-lg ';
-    if (xl_hide) classes += 'hidden-xl ';
+    if (xs_hide) classes += 'xs-hide ';
+    if (sm_hide) classes += 'sm-hide ';
+    if (md_hide) classes += 'md-hide ';
+    if (lg_hide) classes += 'lg-hide ';
+    if (xl_hide) classes += 'xl-hide ';
 
     if (hidden_default) classes += 'hidden-default ';
 
