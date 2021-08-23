9e709cb712e2b730d02341404bcc8487a1336cf6
sureyeahdie <sureyeahdie@gmail.com>
2021-08-23 11:17:29 +0700

    feat([26912313] Magezon: Content Slider): init

--- core/modules/cms/components/cms-renderer/magezon/index.js
+++ core/modules/cms/components/cms-renderer/magezon/index.js
@@ -21,6 +21,7 @@ import MagezonCta from '@core_modules/cms/components/cms-renderer/magezon/Magezo
 import generateCustomCssAnimation from '@core_modules/cms/helpers/magezonCustomCssAnimationGenerator';
 import MagezonSearchForm from '@core_modules/cms/components/cms-renderer/magezon/MagezonSearchForm';
 import MagezonStaticBlock from '@core_modules/cms/components/cms-renderer/magezon/MagezonStaticBlock';
+import MagezonContentSlider from '@core_modules/cms/components/cms-renderer/magezon/MagezonContentSlider';
 import dynamic from 'next/dynamic';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'open-iconic/font/css/open-iconic-bootstrap.css';
@@ -160,6 +161,8 @@ const MagezonElement = (props) => {
             childrenContent = <MagezonCaraousel {...props} />; break;
         case 'search_form':
             childrenContent = <MagezonSearchForm {...props} />; break;
+        case 'content_slider':
+            childrenContent = <MagezonContentSlider {...props} />; break;
         default:
             childrenContent = null;
         }
