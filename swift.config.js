0638878c589df0c568230c652c64af9d7191b9b1
Yosef Kurniawan <jojo@icube.us>
2021-08-24 19:53:14 +0700

    enable checkoutOnly

--- swift.config.js
+++ swift.config.js
@@ -346,7 +346,7 @@ const modules = {
     },
     checkout: {
         enabled: true,
-        checkoutOnly: false,
+        checkoutOnly: true,
         path: '/checkout',
         ipayUrl: 'ipay88/ipayredirect/?orderId=',
         snapUrl: {
