881aff1d154bb148d424d3ee749acdb88c00819d
Yosef Kurniawan <jojo@icube.us>
2021-08-27 16:35:42 +0700

    Disable GTM by default

--- swift.config.js
+++ swift.config.js
@@ -65,12 +65,12 @@ const translation = {
  * NOTE: this GTM functionality includes connecting to GA via GTM tag.
  */
 const GTM = {
-    enable: true,
+    enable: false,
     gtmId: {
         local: '', // sample: GTM-N76V8KQ
         dev: '', // sample: GTM-N76V8KQ
         stage: '', // sample: GTM-N76V8KQ
-        prod: 'GTM-5FQD57V', // sample: GTM-N76V8KQ
+        prod: '', // sample: GTM-N76V8KQ
     },
 };
 
