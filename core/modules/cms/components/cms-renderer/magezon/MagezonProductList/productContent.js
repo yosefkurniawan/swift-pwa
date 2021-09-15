f59acec2eaab41625914cc9e9ce185f05e0d2456
sureyeahdie <sureyeahdie@gmail.com>
2021-09-15 11:52:55 +0700

    temp commit

--- core/modules/cms/components/cms-renderer/magezon/MagezonProductList/productContent.js
+++ core/modules/cms/components/cms-renderer/magezon/MagezonProductList/productContent.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/jsx-one-expression-per-line */
@@ -25,6 +26,7 @@ import { localCompare } from '@services/graphql/schema/local';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { useProduct } from '@core_modules/cms/components/cms-renderer/magezon/MagezonProductList/helpers/productHelper';
 
 const ProductContent = (props) => {
     // prettier-ignore
@@ -59,109 +61,113 @@ const ProductContent = (props) => {
 
     const favoritIcon = wishlist ? <Favorite className={styles.iconShare} /> : <FavoriteBorderOutlined className={styles.iconShare} />;
 
-    React.useEffect(() => {
-        if (!compareList && modules.productcompare.enabled) {
-            const uid_product = getCookies('uid_product_compare');
-            if (uid_product) {
-                getProductCompare({
-                    variables: {
-                        uid: uid_product,
-                    },
-                });
-            }
-        }
-    }, [compareList]);
+    const { handleAddtowishlist, handleClick, handleSetCompareList } = useProduct();
 
-    React.useEffect(() => {
-        if (isLogin && !dataUid && modules.productcompare.enabled) {
-            getUid();
-        }
-    }, [isLogin, dataUid]);
+    console.log(useProduct);
 
-    const handleClick = () => {
-        router.push(url_key);
-    };
+    // React.useEffect(() => {
+    //     if (!compareList && modules.productcompare.enabled) {
+    //         const uid_product = getCookies('uid_product_compare');
+    //         if (uid_product) {
+    //             getProductCompare({
+    //                 variables: {
+    //                     uid: uid_product,
+    //                 },
+    //             });
+    //         }
+    //     }
+    // }, [compareList]);
 
-    const handleAddtowishlist = () => {
-        if (isLogin && isLogin === 1) {
-            addWishlist({
-                variables: {
-                    productId: id,
-                },
-            })
-                .then(async () => {
-                    await setWishlist(!wishlist);
-                    await window.toastMessage({ open: true, variant: 'success', text: t('common:message:feedSuccess') });
-                    router.push('/wishlist');
-                })
-                .catch((e) => {
-                    window.toastMessage({
-                        open: true,
-                        variant: 'error',
-                        text: debuging.originalError ? e.message.split(':')[1] : t('common:message:feedFailed'),
-                    });
-                });
-        } else {
-            window.toastMessage({
-                open: true,
-                variant: 'warning',
-                text: t('catalog:wishlist:addWithoutLogin'),
-            });
-        }
-    };
+    // React.useEffect(() => {
+    //     if (isLogin && !dataUid && modules.productcompare.enabled) {
+    //         getUid();
+    //     }
+    // }, [isLogin, dataUid]);
 
-    const handleSetCompareList = (id_compare) => {
-        const uid_product_compare = getCookies('uid_product_compare');
-        const uids = [];
-        let uid_customer = '';
-        uids.push(id_compare.toString());
-        if (isLogin) {
-            uid_customer = dataUid ? (dataUid.customer.compare_list ? dataUid.customer.compare_list.uid : '') : '';
-        }
-        let isExist = false;
-        if (compareList) {
-            compareList.compareList.items.map((res) => {
-                if (res.uid === id_compare.toString()) {
-                    isExist = true;
-                }
-                return null;
-            });
-            if (!isExist) {
-                addProductCompare({
-                    variables: {
-                        uid: isLogin ? uid_customer : uid_product_compare,
-                        products: uids,
-                    },
-                })
-                    .then(async (res) => {
-                        await window.toastMessage({ open: true, variant: 'success', text: t('common:productCompare:successCompare') });
-                        client.writeQuery({
-                            query: localCompare,
-                            data: {
-                                item_count: res.data.addProductsToCompareList.item_count,
-                            },
-                        });
-                        refetch();
-                        if (isLogin) {
-                            refetchCustomerUid();
-                        }
-                    })
-                    .catch((e) => {
-                        window.toastMessage({
-                            open: true,
-                            variant: 'error',
-                            text: debuging.originalError ? e.message.split(':')[1] : t('common:productCompare:failedCompare'),
-                        });
-                    });
-            } else {
-                window.toastMessage({
-                    open: true,
-                    variant: 'error',
-                    text: t('common:productCompare:existProduct'),
-                });
-            }
-        }
-    };
+    // const handleClick = () => {
+    //     router.push(url_key);
+    // };
+
+    // const handleAddtowishlist = () => {
+    //     if (isLogin && isLogin === 1) {
+    //         addWishlist({
+    //             variables: {
+    //                 productId: id,
+    //             },
+    //         })
+    //             .then(async () => {
+    //                 await setWishlist(!wishlist);
+    //                 await window.toastMessage({ open: true, variant: 'success', text: t('common:message:feedSuccess') });
+    //                 router.push('/wishlist');
+    //             })
+    //             .catch((e) => {
+    //                 window.toastMessage({
+    //                     open: true,
+    //                     variant: 'error',
+    //                     text: debuging.originalError ? e.message.split(':')[1] : t('common:message:feedFailed'),
+    //                 });
+    //             });
+    //     } else {
+    //         window.toastMessage({
+    //             open: true,
+    //             variant: 'warning',
+    //             text: t('catalog:wishlist:addWithoutLogin'),
+    //         });
+    //     }
+    // };
+
+    // const handleSetCompareList = (id_compare) => {
+    //     const uid_product_compare = getCookies('uid_product_compare');
+    //     const uids = [];
+    //     let uid_customer = '';
+    //     uids.push(id_compare.toString());
+    //     if (isLogin) {
+    //         uid_customer = dataUid ? (dataUid.customer.compare_list ? dataUid.customer.compare_list.uid : '') : '';
+    //     }
+    //     let isExist = false;
+    //     if (compareList) {
+    //         compareList.compareList.items.map((res) => {
+    //             if (res.uid === id_compare.toString()) {
+    //                 isExist = true;
+    //             }
+    //             return null;
+    //         });
+    //         if (!isExist) {
+    //             addProductCompare({
+    //                 variables: {
+    //                     uid: isLogin ? uid_customer : uid_product_compare,
+    //                     products: uids,
+    //                 },
+    //             })
+    //                 .then(async (res) => {
+    //                     await window.toastMessage({ open: true, variant: 'success', text: t('common:productCompare:successCompare') });
+    //                     client.writeQuery({
+    //                         query: localCompare,
+    //                         data: {
+    //                             item_count: res.data.addProductsToCompareList.item_count,
+    //                         },
+    //                     });
+    //                     refetch();
+    //                     if (isLogin) {
+    //                         refetchCustomerUid();
+    //                     }
+    //                 })
+    //                 .catch((e) => {
+    //                     window.toastMessage({
+    //                         open: true,
+    //                         variant: 'error',
+    //                         text: debuging.originalError ? e.message.split(':')[1] : t('common:productCompare:failedCompare'),
+    //                     });
+    //                 });
+    //         } else {
+    //             window.toastMessage({
+    //                 open: true,
+    //                 variant: 'error',
+    //                 text: t('common:productCompare:existProduct'),
+    //             });
+    //         }
+    //     }
+    // };
 
     return (
         <>
