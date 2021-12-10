cd89e965b44c9951f8fb0652a4aee991c8ba6c83
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2021-12-10 11:33:05 +0700

    fix([27073001] PDP - [Bug] PDP animate error not stop) : Adjust qty change button on virtual product

--- core/modules/product/pages/default/components/OptionItem/Footer.js
+++ core/modules/product/pages/default/components/OptionItem/Footer.js
@@ -2,7 +2,9 @@ import Typography from '@common_typography';
 import ButtonQty from '@common_buttonqty';
 // import Router from 'next/router';
 import React from 'react';
-import Button from '@common_button';
+import dynamic from 'next/dynamic';
+
+const Button = dynamic(() => import('@common_button'), { ssr: true });
 import useStyles from '@core_modules/product/pages/default/components/OptionItem/style';
 
 const Footer = ({
