ef575036a88d1f75b5ea9bb0abd8bc0b6c88383a
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2021-12-13 11:46:32 +0700

    feat([27050322] General - [Feat] Need capability to set robot meta tag on production to NOINDEX, NOFOLLOW) : Add condition to mitigate error when retrieving default robot config

--- core/modules/theme/layout/index.js
+++ core/modules/theme/layout/index.js
@@ -196,7 +196,7 @@ const Layout = (props) => {
                     name="keywords"
                     content={pageConfig.title ? pageConfig.title : storeConfig.default_title ? storeConfig.default_title : 'Swift Pwa'}
                 />
-                <meta name="robots" content={appEnv === 'prod' ? storeConfig.pwa.default_robot : 'NOINDEX,NOFOLLOW'} />
+                <meta name="robots" content={appEnv === 'prod' && storeConfig.pwa ? storeConfig.pwa.default_robot : 'NOINDEX,NOFOLLOW'} />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="format-detection" content="telephone=no" />
                 <meta name="description" content={ogData['og:description']} />
