288ef433af3dc07073032ae6cf635235744ac89b
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2022-06-16 08:55:47 +0700

    fix([27299888] - [IMPROVE] PWA - header version alternative templates): Support VesMenu for burger menu

---
+++ core/modules/theme/components/header/mobile/VesMenuCategoryList/style.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+    zeroLevel: {
+        paddingLeft: '1rem',
+    },
+    firstLevel: {
+        paddingLeft: '2rem',
+    },
+    secondLevel: {
+        paddingLeft: '3rem',
+    },
+}));
+
+export default useStyles;
