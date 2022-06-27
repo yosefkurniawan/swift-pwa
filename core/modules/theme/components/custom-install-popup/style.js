287466f833570890a50933f78be8d960dab11758
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2022-06-27 09:08:24 +0700

    fix([27299888] - [IMPROVE] PWA - header version alternative templates): Small fix on popup installation

--- core/modules/theme/components/custom-install-popup/style.js
+++ core/modules/theme/components/custom-install-popup/style.js
@@ -77,7 +77,7 @@ const useStyles = makeStyles((theme) => ({
         },
     },
     popupInstallDesktop: {
-        display: 'initial',
+        display: 'inline-block !important',
         position: 'absolute',
         top: '4px',
         fontSize: 13,
