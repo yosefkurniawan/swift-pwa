6c0ea1fb5242ab72d7a1f0dff5fe56d0ff02340f
InuAimar2020 <muhammad.nuh@sirclo.com>
2022-08-19 15:50:26 +0700

    IMPROVE([27285862] Support QA Automation) - fixing feedback element id header

--- core/modules/theme/components/header/desktop/components/v3/mcategory.js
+++ core/modules/theme/components/header/desktop/components/v3/mcategory.js
@@ -113,6 +113,7 @@ const Menu = (props) => {
                             <li
                                 key={idx}
                                 role="menuitem"
+                                id={`header-menuitem-${idx}`}
                                 onMouseEnter={() => {
                                     if (megaMenuRef && val.dropdown_animation_in) {
                                         megaMenuRef.current.classList.add('animate__animated');
