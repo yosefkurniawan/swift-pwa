54b3b94f0f6b11079e23adbd2c517b261b41c9f5
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2022-09-05 12:15:39 +0700

    feat([27358525] - [PWA] Seller Page): Remove duplicate plugin_productlist

--- core/modules/seller/plugins/ProductList/components/ErrorMessage/style.js
+++
@@ -1,8 +0,0 @@
-import makeStyles from '@material-ui/core/styles/makeStyles';
-
-export default makeStyles(() => ({
-    container: {
-        width: '100%',
-        height: '100%',
-    },
-}));
--- core/modules/seller/plugins/ProductList/components/FilterDesktop/index.js
+++
@@ -1,6 +0,0 @@
-import Filter from '@plugin_productlist/components/Filter';
-import View from '@plugin_productlist/components/FilterDesktop/view';
-
-const FilterDialog = (props) => <Filter {...props} FilterView={View} />;
-
-export default FilterDialog;
