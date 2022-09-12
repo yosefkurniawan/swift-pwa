3860c0a56cda8e274e6ef58abc45e414da3d4efb
revqiabdillah <61722669+revqiabdillah@users.noreply.github.com>
2022-09-12 11:52:05 +0700

    ([28834175] Thumbor - moving config) : fixing error declare variabke

--- core/helpers/image.js
+++ core/helpers/image.js
@@ -3,6 +3,7 @@
 export const generateThumborUrl = (src = '', width = 400, height = 400, enable, useHttpsOrHttp, url) => {
     if (enable) {
         let source = src;
+        let newurl = url;
         if (!useHttpsOrHttp) {
             if (source.includes('http')) {
                 source = source.replace('http://', '');
@@ -11,9 +12,9 @@ export const generateThumborUrl = (src = '', width = 400, height = 400, enable, 
                 source = source.replace('https://', '');
             }
         }
-        url = url.replace('width', width);
-        url = url.replace('height', height);
-        return url + source;
+        newurl = newurl.replace('width', width);
+        newurl = newurl.replace('height', height);
+        return newurl + source;
     }
 
     return src;
