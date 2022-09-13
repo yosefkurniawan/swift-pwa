56cd543b1ff77c6064868fb2ef2cee51477389e9
InuAimar2020 <muhammad.nuh@sirclo.com>
2022-09-13 17:51:57 +0700

    BUG([28829742] - GA4 is not working properly)

--- core/modules/catalog/plugins/ProductList/core.js
+++ core/modules/catalog/plugins/ProductList/core.js
@@ -146,6 +146,7 @@ const Product = (props) => {
 
     React.useEffect(() => {
         if (data && data.products) {
+            // GTM UA dataLayer
             const tagManagerArgs = {
                 dataLayer: {
                     event: 'impression',
@@ -173,7 +174,42 @@ const Product = (props) => {
                     },
                 },
             };
+            // GA 4 dataLayer
+            const tagManagerArgsGA4 = {
+                dataLayer: {
+                    event: 'view_item_list',
+                    pageName: categoryPath,
+                    pageType: 'category',
+                    ecommerce: {
+                        items: data.products.items.map((product, index) => {
+                            let categoryProduct = '';
+                            let categoryOne = '';
+                            let categoryTwo = '';
+                            // eslint-disable-next-line no-unused-expressions
+                            product.categories.length > 0 && (
+                                categoryOne = product.categories[0].name,
+                                categoryTwo = product.categories[1]?.name,
+                                product.categories.map(({ name }, indx) => {
+                                    if (indx > 0) categoryProduct += `/${name}`;
+                                    else categoryProduct += name;
+                                })
+                            );
+                            return {
+                                item_name: product.name,
+                                item_id: product.sku,
+                                price: product.price_range.minimum_price.regular_price.value,
+                                item_category: categoryOne,
+                                item_category_2: categoryTwo,
+                                item_list_name: categoryProduct,
+                                index,
+                                currency: product.price_range.minimum_price.regular_price.currency,
+                            };
+                        }),
+                    },
+                },
+            };
             TagManager.dataLayer(tagManagerArgs);
+            TagManager.dataLayer(tagManagerArgsGA4);
         }
     }, [data]);
 
