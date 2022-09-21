bfcd5b5f75759aa683ada351a363bc617fd55e5a
revqiabdillah <rifqi.abdillah@sirclo.com>
2022-09-21 10:19:26 +0700

    remove patch bug loadmore

--- patch.sh
+++ patch.sh
@@ -2,5 +2,4 @@
 cd /usr/src/app
 
 ######################### START of line patch ############################
-patch -p1 --forward < patches/fix_loadmore_plp.patch || true
 ######################### END of line patch ##############################
