f6cfbf807585bcc11cf37dd2a4275d9500c290b4
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2022-09-22 17:46:43 +0700

    feat([27358527] - [PWA] Split Checkout by vendor): Fix issue when no shipping is available

--- core/modules/checkout/pages/default/components/shipping/view.js
+++ core/modules/checkout/pages/default/components/shipping/view.js
@@ -387,7 +387,7 @@ const ShippingView = (props) => {
                                                                     }}
                                                                     isShipping
                                                                 />
-                                                            ) : null}
+                                                            ) : <Typography variant="p">{t('checkout:noShipping')}</Typography>}
                                                         </div>
                                                     </AccordionDetails>
                                                 </Accordion>
@@ -481,15 +481,7 @@ const ShippingView = (props) => {
                 );
             }
         } else {
-            content = (
-                <Radio
-                    value={selected.shipping}
-                    onChange={handleShipping}
-                    classContainer={styles.listShipping}
-                    CustomItem={DeliveryItem}
-                    valueData={data.shippingMethods}
-                />
-            );
+            content = <Typography variant="p">{t('checkout:noShipping')}</Typography>;
         }
     } else if (loadingSellerInfo) {
         content = <Loader />;
