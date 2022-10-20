195791e122ca45742425b31c7c86071947c0fef3
AndyCungkrinx <57337592+AndyCungkrinx@users.noreply.github.com>
2022-10-20 14:19:29 +0700

    Update build.sh

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -9,6 +9,6 @@ ts=$(date +%s)
 githead=$(git rev-list -1 tags/$2)
 set -ex
 echo "Building Backend Docker image with tag " $$githead-$$ts
-docker login -u _json_key -p "$(cat /etc/service_key/service-account-key.json | tr '\n' ' ')" https://asia.gcr.io.
+docker login -u _json_key -p "$(cat /etc/service_key/service-account-key.json | tr '\n' ' ')" https://asia.gcr.io
 docker build -t asia.gcr.io/sirclo-iii-nonprod/$1:$2-$ts -f Dockerfile.swift-pwa .
 docker push asia.gcr.io/sirclo-iii-nonprod/$1:$2-$ts
