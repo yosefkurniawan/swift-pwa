a969ea76dcd33fd2ccae4d3ac7f96722e9d41303
InuAimar2020 <muhammad.nuh@sirclo.com>
2022-10-26 10:52:56 +0700

    BUG([28891054] Language switcher does not work)

--- core/modules/setting/components/language/view.js
+++ core/modules/setting/components/language/view.js
@@ -4,6 +4,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Button from '@material-ui/core/Button';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -19,7 +20,7 @@ const useStyles = makeStyles(() => ({
 
 const ViewSwitcherLanguage = (props) => {
     const {
-        t, title, id, open, anchorEl, handleClick, handleClose, dataLang, lang, onClickLanguage,
+        t, title, id, open, anchorEl, handleClick, handleClose, dataLang, lang, onClickLanguage, loadDataLang,
     } = props;
     const buttonRef = useRef();
     const classes = useStyles();
@@ -29,12 +30,10 @@ const ViewSwitcherLanguage = (props) => {
     const styleTitle = { fontSize: 12, textTransform: 'uppercase' };
     const styleButton = { fontFamily: 'Montserrat', padding: '0px', fontSize: title ? '12px' : '1em' };
     const listDataLanguage = [];
+
     if (dataLang !== undefined) {
         dataLang.map((item) => {
-            const isCurrent = item.value === lang.value;
-            if (!isCurrent) {
-                listDataLanguage.push(item);
-            }
+            listDataLanguage.push(item);
             return item;
         });
     }
@@ -44,43 +43,53 @@ const ViewSwitcherLanguage = (props) => {
      */
     return (
         <div>
-            {/* [CURRENCY] TITLE */}
-            {title && (
+            {loadDataLang && (
                 <div>
-                    <strong style={styleTitle}>{title}</strong>
+                    <Skeleton style={{ padding: 0 }} variant="rect" width={100} height={10} />
+                    <Skeleton style={{ display: 'inline-block', padding: 0 }} variant="rect" width={100} height={10} />
                 </div>
             )}
+            {!loadDataLang && dataLang !== undefined && (
+                <div>
+                    {/* [CURRENCY] TITLE */}
+                    {title && (
+                        <div>
+                            <strong style={styleTitle}>{title}</strong>
+                        </div>
+                    )}
 
-            {/* [CURRENCY] BUTTON */}
-            <Button ref={buttonRef} onClick={handleClick} style={styleButton}>
-                {t('common:setting:language')}
-                :&nbsp;
-                <strong>{lang?.label}</strong>
-            </Button>
+                    {/* [CURRENCY] BUTTON */}
+                    <Button ref={buttonRef} onClick={handleClick} style={styleButton}>
+                        {t('common:setting:store')}
+                        :&nbsp;
+                        <strong>{lang?.label}</strong>
+                    </Button>
 
-            {/* [CURRENCY] LIST */}
-            {
-                listDataLanguage && listDataLanguage.length > 0 && (
-                    <Popover
-                        id={id}
-                        open={open}
-                        anchorEl={anchorEl}
-                        onClose={handleClose}
-                        anchorOrigin={anchorOrigin}
-                        transformOrigin={transforOrigin}
-                        container={buttonRef.current}
-                    >
-                        <List component="nav">
-                            {listDataLanguage !== undefined
+                    {/* [CURRENCY] LIST */}
+                    {
+                        listDataLanguage && listDataLanguage.length > 0 && (
+                            <Popover
+                                id={id}
+                                open={open}
+                                anchorEl={anchorEl}
+                                onClose={handleClose}
+                                anchorOrigin={anchorOrigin}
+                                transformOrigin={transforOrigin}
+                                container={buttonRef.current}
+                            >
+                                <List component="nav">
+                                    {listDataLanguage !== undefined
                             && listDataLanguage.map((item, index) => (
                                 <ListItem button key={`language-${index}`} onClick={() => onClickLanguage({ item })}>
                                     <ListItemText classes={{ primary: classes.listItemText }} primary={`${item.label}`} />
                                 </ListItem>
                             ))}
-                        </List>
-                    </Popover>
-                )
-            }
+                                </List>
+                            </Popover>
+                        )
+                    }
+                </div>
+            )}
         </div>
     );
 };
