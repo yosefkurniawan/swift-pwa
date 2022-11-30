ef5ead6d30f8c1dc38b7253a09b229e939ec7f83
Asih Priyatno <asih.priyatno@sirclo.com>
2022-11-30 08:05:14 +0700

    feat([Request Config] change host): change host flexible from env

--- core/api/rest/request/index.js
+++ core/api/rest/request/index.js
@@ -1,6 +1,11 @@
+const { getAppEnv } = require('../../../helpers/env');
+const { HOST } = require('../../../../swift.config');
+
 function requestInternal() {
+    const appEnv = getAppEnv();
+    const url = HOST[appEnv] || HOST.prod;
     return new Promise((resolve) => {
-        fetch('http://localhost:3000/getConfig')
+        fetch(`${url}/getConfig`)
             .then((response) => response.json())
             .then((data) => resolve(data));
     });
