b985ad5ca319d9357a0afb14ca2c2f63ea15e204
yosefkurniawan <yosef.koko@sirclo.com>
2023-01-09 18:51:21 +0700

    Update project script

--- project.sh
+++ project.sh
@@ -1,45 +1,75 @@
+#!/bin/bash
+
+GIT_URL="https://github.com/icube-mage/swift-pwa-demo.git"
+PROJECT_NAME="swift-pwa-demo"
+
+function resetRoot() {
+    rm -rf src/* pages/* public/* patches/*
+    rm -rf .env
+    rm project_patch.sh
+    git checkout src/* pages/* public/* patches/* swift.config.js
+    git clean -f
+}
+
+function sync() {
+    resetRoot
+    cp -rf project/$PROJECT_NAME/src/* src/
+    cp -rf project/$PROJECT_NAME/pages/* pages/
+    cp -rf project/$PROJECT_NAME/public/* public/
+    cp -rf project/$PROJECT_NAME/patches/* patches/
+    cp -rf project/$PROJECT_NAME/swift.config.js .
+    cp -rf project/$PROJECT_NAME/project_patch.sh .
+    cp -rf project/$PROJECT_NAME/.env .
+}
+
+function applyPatch() {
+    cp -rf project/$PROJECT_NAME/src/* src/
+    cp -rf project/$PROJECT_NAME/pages/* pages/
+    cp -rf project/$PROJECT_NAME/public/* public/
+    cp -rf project/$PROJECT_NAME/patches/* patches/
+    cp -rf project/$PROJECT_NAME/swift.config.js .
+    cp -rf project/$PROJECT_NAME/project_patch.sh .
+    cp -rf project/$PROJECT_NAME/.env .
+}
+
 while true; do
 case "$1" in
     # -i|--install
-	-i|--install) MSG='Install Project '$2; shift
+	-i|--install) MSG='Install Project '$GIT_URL; shift
 	    echo "$MSG"
-        # $1 = repo url. eg: https://github.com/yosefkurniawan/swift-pwa-custom.git
-        # $2 = project name in the repo url. eg: swift-pwa-custom
-        
-        # reset root pwa repo
-        rm pages/*
-        rm public/*
-        rm patches/*
-        git checkout pages/ public/ patches/
 
         # install project repo
         mkdir -p project
-        rm -rf src swift.config.js .env
-        git clone $2 project/$3
+        git clone $GIT_URL project/$PROJECT_NAME
         
-        # copy files
-        cp -rf project/$3/* .
+        # sync
+        sync
+
+        #apply patch
+        applyPatch
+        ;;
+
+    # -u|--uninstall
+	-u|--uninstall) MSG='Uninstall Project '$PROJECT_NAME; shift
+	    echo "$MSG"
+        
+        # reset root pwa repo
+        resetRoot
+
+        # uninstall project repo
+        rm -rf project
         ;;
 
      # -s|--sync
 	-s|--sync) MSG='Sync Project Files '$2; shift
 	    echo "$MSG"
-        
-        # reset root pwa repo
-        rm pages/*
-        rm public/*
-        rm patches/*
-        git checkout pages/ public/ patches/
-        rm -rf src swift.config.js .env
-        
-        # copy files
-        cp -rf project/*/* .
+        sync
         ;;
     
     # -p|--patch
 	-p|--patch) MSG='Apply patches '$2; shift
 	    echo "$MSG"
-        
+        applyPatch
         ;;
     
     # --help
