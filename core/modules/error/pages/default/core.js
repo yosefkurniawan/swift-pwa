0a99de694ee26b634cd4dfd8e74d649bb1655d64
noerayyu <noer.ayu@sirclo.com>
2023-02-03 13:15:33 +0700

    fix([29010068] error - page):add isLogin props in error page layout

--- core/modules/error/pages/default/core.js
+++ core/modules/error/pages/default/core.js
@@ -1,16 +1,21 @@
 import Layout from '@layout';
 import { withTranslation } from '@i18n';
 import * as Sentry from '@sentry/node';
+import { getLoginInfo } from '@helper_auth';
 
 const Error = (props) => {
     const {
         statusCode, Content, pageConfig, storeConfig, hasGetInitialPropsRun, err,
     } = props;
+    let isLogin;
     if (!hasGetInitialPropsRun && err) {
         // getInitialProps is not called in case of
         // https://github.com/vercel/next.js/issues/8592. As a workaround, we pass
         // err via _app.js so it can be captured
         Sentry.captureException(err);
+    }
+    if (typeof window !== 'undefined') {
+        isLogin = getLoginInfo();
     }
     const statusCodes = {
         400: 'Bad Request',
@@ -26,7 +31,7 @@ const Error = (props) => {
         title,
     };
     return (
-        <Layout pageConfig={pageConfig || config} {...props} {...storeConfig}>
+        <Layout pageConfig={pageConfig || config} isLogin={isLogin} {...props} {...storeConfig}>
             <Content statusCode={statusCode} title={title} />
         </Layout>
     );
