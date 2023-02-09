8322a4ac482a51ba22239f7b8c740b84570185ea
yosefkurniawan <yosef.koko@sirclo.com>
2023-02-09 12:06:46 +0700

    IMPROVE([28901402] Memory): improve getPath helper to wreturn the same value for ssr & csr

--- core/helpers/getPath.js
+++ core/helpers/getPath.js
@@ -1,24 +1,6 @@
-import { graphqlEndpoint } from '@config';
-import { getAppEnv } from './env';
-
 const getPath = (href = '') => {
-    if (href && href !== '' && typeof window !== 'undefined') {
-        const link = document.createElement('a');
-        link.href = href;
-        let path = link.pathname;
-        if (path.includes('.html')) {
-            path = path.split('.html');
-            // eslint-disable-next-line prefer-destructuring
-            path = path[0];
-        }
-        return path;
-    }
-    const appEnv = getAppEnv();
-    const env = appEnv === 'local' ? 'dev' : appEnv;
-    let url = graphqlEndpoint[env] || 'dev';
-    url = url.replace('/graphql', '');
-    let path = href.replace('.html', '');
-    path = path.replace(url, '');
+    const checkUrl = new URL(href);
+    const path = checkUrl.pathname.replace('.html', '');
     return path;
 };
 
