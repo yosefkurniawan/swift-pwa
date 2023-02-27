7890f89ad00e91a0719788182c8fc0477b753302
noerayyu <noer.ayu@sirclo.com>
2023-02-27 09:02:13 +0700

    IMPROVE([29046435] Common - LazyImage):add useRef for mounting in useEffect

--- core/modules/commons/Image/LazyImage.js
+++ core/modules/commons/Image/LazyImage.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, {
+    useState, useEffect, useCallback, useRef,
+} from 'react';
 
 const placeHolder = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAACCAQAAADVobXoAAAAEElEQVR42mM8858BDBhhDAArAgOZPzQZFwAAAABJRU5ErkJggg==';
 
@@ -14,60 +16,63 @@ const LazyImage = ({ src, alt, style = {} }) => {
         event.target.classList.add('has-error');
     }, []);
 
+    const mount = useRef(null);
     useEffect(
         () => {
+            mount.current = true;
             let observer;
             let didCancel = false;
-
-            if (typeof window !== 'undefined' && navigator && navigator?.appVersion) {
-                const userAgent = navigator.appVersion;
-                const regex = (/iPhone|iPad|iPod|Mac/i);
-                const isIOS = regex.test(userAgent);
-                if (isIOS) {
-                    const version = userAgent.match(/\b[0-9]+_[0-9]+(?:_[0-9]+)?\b/)[0];
-                    const majorVersion = version.split('_')[0];
-                    // intersectionObserver not supported on IOS version 14 and below
-                    if (majorVersion < 14) {
+            if (mount.current) {
+                if (typeof window !== 'undefined' && navigator && navigator?.appVersion) {
+                    const userAgent = navigator.appVersion;
+                    const regex = (/iPhone|iPad|iPod|Mac/i);
+                    const isIOS = regex.test(userAgent);
+                    if (isIOS) {
+                        const version = userAgent.match(/\b[0-9]+_[0-9]+(?:_[0-9]+)?\b/)[0];
+                        const majorVersion = version.split('_')[0];
+                        // intersectionObserver not supported on IOS version 14 and below
+                        if (majorVersion < 14) {
                         // Old browsers fallback
-                        setImageSrc(src);
-                    } else if (imageRef && imageSrc !== src) {
-                        if (IntersectionObserver) {
-                            observer = new IntersectionObserver(
-                                (entries) => {
-                                    entries.forEach((entry) => {
-                                        if (
-                                            !didCancel
-                                  && (entry.intersectionRatio > 0 || entry.isIntersecting)
-                                        ) {
-                                            setImageSrc(src);
-                                            observer.unobserve(imageRef);
-                                        }
-                                    });
-                                },
-                                {
-                                    threshold: 0.01,
-                                    rootMargin: '75%',
-                                },
-                            );
-                            observer.observe(imageRef);
-                        } else {
-                            // Old browsers fallback
                             setImageSrc(src);
+                        } else if (imageRef && imageSrc !== src) {
+                            if (IntersectionObserver) {
+                                observer = new IntersectionObserver(
+                                    (entries) => {
+                                        entries.forEach((entry) => {
+                                            if (
+                                                !didCancel
+                                && (entry.intersectionRatio > 0 || entry.isIntersecting)
+                                            ) {
+                                                setImageSrc(src);
+                                                observer.unobserve(imageRef);
+                                            }
+                                        });
+                                    },
+                                    {
+                                        threshold: 0.01,
+                                        rootMargin: '75%',
+                                    },
+                                );
+                                observer.observe(imageRef);
+                            } else {
+                            // Old browsers fallback
+                                setImageSrc(src);
+                            }
                         }
                     }
                 }
             }
-
             return () => {
                 didCancel = true;
                 // on component cleanup, we remove the listner
                 if (observer && observer.unobserve) {
                     observer.unobserve(imageRef);
                 }
+                mount.current = false;
             };
-        },
-        [src, imageSrc, imageRef],
+        }, [src, imageSrc, imageRef],
     );
+
     return (
         <>
             <img
