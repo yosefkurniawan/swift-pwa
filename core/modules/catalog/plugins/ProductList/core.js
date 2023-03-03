f2fa7a04ce5b78c7dcfee6456b426e6e06a6dc6a
noerayyu <noer.ayu@sirclo.com>
2023-03-03 09:48:47 +0700

    IMPROVE([29046435] Plugin - product list):place GA4 function outside the components

--- core/modules/catalog/plugins/ProductList/core.js
+++ core/modules/catalog/plugins/ProductList/core.js
@@ -26,6 +26,67 @@ import { useReactiveVar } from '@apollo/client';
 
 const generateIdentifier = (page, path) => `page_${page}_${path}`;
 
+const getTagManager = (categoryPath, storeConfig, data) => ({
+        dataLayer: {
+            event: 'impression',
+            eventCategory: 'Ecommerce',
+            eventAction: 'Impression',
+            eventLabel: categoryPath ? `category ${categoryPath}` : '',
+            ecommerce: {
+                currencyCode: storeConfig && storeConfig.base_currency_code ? storeConfig.base_currency_code : 'IDR',
+                impressions: data.products.items.map((product, index) => {
+                    let categoryProduct = '';
+                    // eslint-disable-next-line no-unused-expressions
+                    product.categories.length > 0 &&
+                        product.categories.map(({ name }, indx) => {
+                            if (indx > 0) categoryProduct += `/${name}`;
+                            else categoryProduct += name;
+                        });
+                    return {
+                        name: product.name,
+                        id: product.sku,
+                        category: categoryProduct,
+                        price: product.price_range.minimum_price.regular_price.value,
+                        list: categoryProduct,
+                        position: index,
+                    };
+                }),
+            },
+        },
+    });
+const getTagManagerGA4 = (categoryPath, data) => ({
+        dataLayer: {
+            event: 'view_item_list',
+            pageName: categoryPath,
+            pageType: 'category',
+            ecommerce: {
+                items: data.products.items.map((product, index) => {
+                    let categoryProduct = '';
+                    let categoryOne = '';
+                    let categoryTwo = '';
+                    // eslint-disable-next-line no-unused-expressions
+                    product.categories.length > 0 &&
+                        ((categoryOne = product.categories[0].name),
+                        (categoryTwo = product.categories[1]?.name),
+                        product.categories.map(({ name }, indx) => {
+                            if (indx > 0) categoryProduct += `/${name}`;
+                            else categoryProduct += name;
+                        }));
+                    return {
+                        item_name: product.name,
+                        item_id: product.sku,
+                        price: product.price_range.minimum_price.regular_price.value,
+                        item_category: categoryOne,
+                        item_category_2: categoryTwo,
+                        item_list_name: categoryProduct,
+                        index,
+                        currency: product.price_range.minimum_price.regular_price.currency,
+                    };
+                }),
+            },
+        }
+    });
+
 const ProductPagination = (props) => {
     const {
         catId = 0,
@@ -338,67 +399,9 @@ const ProductPagination = (props) => {
         if (data && data.products) {
             setProducts(data.products);
             // GTM UA dataLayer
-            const tagManagerArgs = {
-                dataLayer: {
-                    event: 'impression',
-                    eventCategory: 'Ecommerce',
-                    eventAction: 'Impression',
-                    eventLabel: categoryPath ? `category ${categoryPath}` : '',
-                    ecommerce: {
-                        currencyCode: storeConfig && storeConfig.base_currency_code ? storeConfig.base_currency_code : 'IDR',
-                        impressions: data.products.items.map((product, index) => {
-                            let categoryProduct = '';
-                            // eslint-disable-next-line no-unused-expressions
-                            product.categories.length > 0 &&
-                                product.categories.map(({ name }, indx) => {
-                                    if (indx > 0) categoryProduct += `/${name}`;
-                                    else categoryProduct += name;
-                                });
-                            return {
-                                name: product.name,
-                                id: product.sku,
-                                category: categoryProduct,
-                                price: product.price_range.minimum_price.regular_price.value,
-                                list: categoryProduct,
-                                position: index,
-                            };
-                        }),
-                    },
-                },
-            };
+            const tagManagerArgs = getTagManager(categoryPath, storeConfig, data);
             // GA 4 dataLayer
-            const tagManagerArgsGA4 = {
-                dataLayer: {
-                    event: 'view_item_list',
-                    pageName: categoryPath,
-                    pageType: 'category',
-                    ecommerce: {
-                        items: data.products.items.map((product, index) => {
-                            let categoryProduct = '';
-                            let categoryOne = '';
-                            let categoryTwo = '';
-                            // eslint-disable-next-line no-unused-expressions
-                            product.categories.length > 0 &&
-                                ((categoryOne = product.categories[0].name),
-                                (categoryTwo = product.categories[1]?.name),
-                                product.categories.map(({ name }, indx) => {
-                                    if (indx > 0) categoryProduct += `/${name}`;
-                                    else categoryProduct += name;
-                                }));
-                            return {
-                                item_name: product.name,
-                                item_id: product.sku,
-                                price: product.price_range.minimum_price.regular_price.value,
-                                item_category: categoryOne,
-                                item_category_2: categoryTwo,
-                                item_list_name: categoryProduct,
-                                index,
-                                currency: product.price_range.minimum_price.regular_price.currency,
-                            };
-                        }),
-                    },
-                },
-            };
+            const tagManagerArgsGA4 = getTagManagerGA4(categoryPath, data);
             TagManager.dataLayer(tagManagerArgs);
             TagManager.dataLayer(tagManagerArgsGA4);
         }
@@ -649,67 +652,9 @@ const ProductLoadMore = (props) => {
         if (data && data.products) {
             setProducts(data.products);
             // GTM UA dataLayer
-            const tagManagerArgs = {
-                dataLayer: {
-                    event: 'impression',
-                    eventCategory: 'Ecommerce',
-                    eventAction: 'Impression',
-                    eventLabel: categoryPath ? `category ${categoryPath}` : '',
-                    ecommerce: {
-                        currencyCode: storeConfig && storeConfig.base_currency_code ? storeConfig.base_currency_code : 'IDR',
-                        impressions: data.products.items.map((product, index) => {
-                            let categoryProduct = '';
-                            // eslint-disable-next-line no-unused-expressions
-                            product.categories.length > 0 &&
-                                product.categories.map(({ name }, indx) => {
-                                    if (indx > 0) categoryProduct += `/${name}`;
-                                    else categoryProduct += name;
-                                });
-                            return {
-                                name: product.name,
-                                id: product.sku,
-                                category: categoryProduct,
-                                price: product.price_range.minimum_price.regular_price.value,
-                                list: categoryProduct,
-                                position: index,
-                            };
-                        }),
-                    },
-                },
-            };
+            const tagManagerArgs = getTagManager(categoryPath, storeConfig, data);
             // GA 4 dataLayer
-            const tagManagerArgsGA4 = {
-                dataLayer: {
-                    event: 'view_item_list',
-                    pageName: categoryPath,
-                    pageType: 'category',
-                    ecommerce: {
-                        items: data.products.items.map((product, index) => {
-                            let categoryProduct = '';
-                            let categoryOne = '';
-                            let categoryTwo = '';
-                            // eslint-disable-next-line no-unused-expressions
-                            product.categories.length > 0 &&
-                                ((categoryOne = product.categories[0].name),
-                                (categoryTwo = product.categories[1]?.name),
-                                product.categories.map(({ name }, indx) => {
-                                    if (indx > 0) categoryProduct += `/${name}`;
-                                    else categoryProduct += name;
-                                }));
-                            return {
-                                item_name: product.name,
-                                item_id: product.sku,
-                                price: product.price_range.minimum_price.regular_price.value,
-                                item_category: categoryOne,
-                                item_category_2: categoryTwo,
-                                item_list_name: categoryProduct,
-                                index,
-                                currency: product.price_range.minimum_price.regular_price.currency,
-                            };
-                        }),
-                    },
-                },
-            };
+            const tagManagerArgsGA4 = getTagManagerGA4(categoryPath, data);
             TagManager.dataLayer(tagManagerArgs);
             TagManager.dataLayer(tagManagerArgsGA4);
         }
