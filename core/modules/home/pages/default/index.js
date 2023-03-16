9da8c1c36f5209d013460763d421261ea82c8da9
noerayyu <noer.ayu@sirclo.com>
2023-03-16 08:05:46 +0700

    fix([29078263] Homepage - SSR):change config from data graphql

--- core/modules/home/pages/default/index.js
+++ core/modules/home/pages/default/index.js
@@ -6,17 +6,26 @@ import dynamic from 'next/dynamic';
 import { modules, keyLocalStorage } from '@config';
 import graphRequest from '@graphql_request';
 import { getHomePageConfig } from '@core_modules/home/service/graphql/schema';
-import { getLocalStorage } from '@helper_localstorage';
 
 const Page = dynamic(() => ((!modules.checkout.checkoutOnly)
     ? import('@core_modules/home/pages/default/core')
     : import('@module_checkout/pages/default')));
 
 Page.getInitialProps = async (ctx) => {
+    let homePageConfig;
+
+    if (!modules.checkout.checkoutOnly && ctx && ctx.req) {
+        homePageConfig = await graphRequest(getHomePageConfig);
+    } else if (!modules.checkout.checkoutOnly && typeof window !== 'undefined') {
+        if (!homePageConfig) {
+            homePageConfig = await graphRequest(getHomePageConfig);
+        }
+    }
     return {
         namespacesRequired: modules.checkout.checkoutOnly
             ? ['common', 'checkout', 'customer', 'validate']
             : ['common', 'home'],
+        homePageConfig,
     };
 };
 
