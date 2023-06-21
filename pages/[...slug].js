5d0d72bf1148a86bb107845b6c57c7040ccbf119
Rifqi Fauzi Rahmadzani <rifqifai@Rifqifai-2.local>
2023-06-21 12:10:36 +0700

    change getInitialProps to getServerSideProps under modules to pages

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,3 +1,36 @@
 import Page from '@core_modules/slug';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { getCmsList } from '@services/graphql/schema/config';
+import graphRequest from '@graphql_request';
+import { storeConfigNameCookie } from '@config';
+
+export async function getServerSideProps(ctx, req) {
+    // const {
+    //     req,
+    // } = ctx;
+
+    let cmsList = {};
+    if (typeof window === 'undefined' && !req?.cookies[storeConfigNameCookie]) {
+        cmsList = await graphRequest(getCmsList);
+    }
+    const allcookie = req ? req.cookies : {};
+    const obj = {
+        slug: req.query.slug,
+        ...(await serverSideTranslations(ctx.locale, ['common', 'product', 'category', 'validate', 'catalog'])),
+        token: req && req.session ? req.session.token : '',
+        isLogin: allcookie.isLogin || 0,
+        url_key: req
+            ? `${req.protocol}://${req.get('host')}`
+            : `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}`,
+    };
+
+    obj.cms_page = cmsList.storeConfig && cmsList.storeConfig.cms_page ? cmsList.storeConfig.cms_page : '';
+
+    return {
+        props: {
+            ...obj,
+        },
+    };
+};
 
 export default Page;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,34 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-unused-vars */
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { modules } from '@config';
+import graphRequest from '@graphql_request';
+import { getHomePageConfig } from '@core_modules/home/service/graphql/schema';
+import { storeConfigVar } from '@root/core/services/graphql/cache';
+
 import Page from '@core_modules/home/pages/default';
+
+export async function getServerSideProps(ctx) {
+    let homePageConfig;
+
+    if (!modules.checkout.checkoutOnly && ctx && ctx.req) {
+        const homeConfig = await graphRequest(getHomePageConfig);
+        homePageConfig = homeConfig.storeConfig;
+    } else if (!modules.checkout.checkoutOnly && typeof window !== 'undefined') {
+        homePageConfig = storeConfigVar();
+        if (!homePageConfig) {
+            const homeConfig = await graphRequest(getHomePageConfig);
+            homePageConfig = homeConfig.storeConfig;
+        }
+    }
+    return {
+        props: {
+            ...(await serverSideTranslations(ctx.locale, modules.checkout.checkoutOnly
+                ? ['common', 'checkout', 'customer', 'validate']
+                : ['common', 'home'])),
+            homePageConfig,
+        },
+    };
+};
 
 export default Page;
