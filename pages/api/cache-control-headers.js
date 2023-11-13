e0427cd532a06db075a2f659cd49d934b0a6a48f
yosefkurniawan <yosef.koko@sirclo.com>
2023-11-13 16:42:46 +0700

    Revert "add cache-control"
    This reverts commit eb26c39e36c042153224ba7172d1f9134156b61d.

--- pages/api/cache-control-headers.js
+++
@@ -1,7 +0,0 @@
-export default function handler(response) {
-    response.setHeader('Vercel-CDN-Cache-Control', 'max-age=3600');
-    response.setHeader('CDN-Cache-Control', 'max-age=60');
-    response.setHeader('Cache-Control', 'max-age=10');
-
-    return response.status(200).json({ name: 'John Doe' });
-}
